4b5ad1e6c50543239cdcea7beaffd8a9012f769c
Johannes Bahne <johannes.bahne@tu-dortmund.de>
2018-04-28 21:13:43 +0200

    implemented method to induce SA00 with DC

---
+++ sacabench/util/induce_sa_dc.hpp
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * util/induce_sa_dc.hpp
+ *
+ * Copyright (C) 2018 Nico Bertram <nico.bertram@tu-dortmund.de>
+ * Copyright (C) 2018 Johannes Bahne <johannes.bahne@tu-dortmund.de>
+ * 
+ * All rights reserved. Published under the BSD-3 license in the LICENSE file.
+ ******************************************************************************/
+#include <tuple>
+#include <array>
+#include <iostream>
+#pragma once
+
+namespace sacabench::util {
+  template<typename T, typename C, typename I, typename S> 
+  //template T for input string
+  //template C for input characters
+  //template I for ISA
+  //template C for SA
+
+  /**\brief Identify order of chars starting in position i mod 3 = 0 with difference cover
+  * \param input input text
+  * \param isa12 calculated ISA for triplets beginning in i mod 3 != 0
+  * \param sa0 memory block for resulting SA for positions beginning in i mod 3 = 0
+  * 
+  * This method identifies the order of the characters in input_string in positions i mod 3 = 0 
+  * with information of ranks of triplets starting in position i mod 3 != 0 of input string.
+  * This method works correct because of the difference cover idea.
+  */
+  void induce_sa_dc(T& input, I& isa12, S& sa0) {
+    
+    static_assert(sa0.size() > 0, "Suffix Array has to be initialised.");
+    
+    std::array<std::tuple<C,int>, sa0.size()> sa0_to_be_sorted;
+    
+    for(int i = 0; i < sa0.size(); i++) {
+      sa0_to_be_sorted[i] = std::tuple(input[3*i], isa12[i]);
+    }  
+    
+    //TODO: sort Tupels with radix_sort
+    //radix_sort(sa0_to_be_sorted, sa0);
+  }
+}  // namespace sacabench::util
+
+/******************************************************************************/
