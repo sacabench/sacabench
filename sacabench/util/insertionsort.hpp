6b8bea816e6f8551105606b15306a75ec17d8b20
rlpink <rosa.pink@tu-dortmund.de>
2018-04-30 18:14:19 +0200

    Anpassung der Vergleichsfkt (von Marvin L.)

--- sacabench/util/insertionsort.hpp
+++ sacabench/util/insertionsort.hpp
@@ -11,11 +11,19 @@
 
 namespace sacabench::util{
 
-  template<typename T> void insertion_sort(span<T> A){
+
+struct greater_than {
+  template<typename T>
+  bool operator()(T const& a, T const& b) {
+    return a > b;
+  }
+};
+
+  template<typename T, typename F = greater_than> void insertion_sort(span<T> A, F compare_fun = F()){
     for(size_t i = 1; i < A.size(); i++) {
       auto to_sort = A[i];
       auto j = i;
-      while((j > 0) && (A[j-1] > to_sort)) {
+      while((j > 0) && compare_fun(A[j-1], to_sort)) {
         A[j] = A[j - 1];
         j = j - 1;
       }
