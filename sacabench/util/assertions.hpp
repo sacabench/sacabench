b8d3dbbc4537016327a1fdb97563835305927a26
Marvin Löbel <loebel.marvin@gmail.com>
2018-05-01 10:37:59 +0200

    Unify codestyle a bit

--- sacabench/util/assertions.hpp
+++ sacabench/util/assertions.hpp
@@ -7,8 +7,8 @@ namespace sacabench::util {
 #ifdef DEBUG
 #ifndef DCHECK
 /// Internal macro for checking a boolean value
-/// and printing two comparison values.
-#define DCHECK_(x,y,z)                                           \
+/// and printing the two comparison values.
+#define DCHECK_BINARY(x,y,z)                                     \
     if (!(x)) {                                                  \
         std::sstream msg;                                        \
         msg << " in file " << __FILE__ << ":" << __LINE__;       \
@@ -24,7 +24,8 @@ namespace sacabench::util {
         msg << ("the check failed: " #x) ;                       \
         throw std::runtime_error(msg.str());                     \
     }
-/// Macro for checking a boolean value, and printing a custom error message
+/// Macro for checking a boolean value
+/// and printing a custom error message
 #define DCHECK_MSG(x, s)                                         \
     if (!(x)) {                                                  \
         std::sstream msg;                                        \
@@ -36,22 +37,22 @@ namespace sacabench::util {
 #endif //DCHECK
 #else //DEBUG
 // Define macros as empty
-#define DCHECK_(x,y,z)
+#define DCHECK_BINARY(x,y,z)
 #define DCHECK(x)
 #define DCHECK_MSG(x)
 #endif //DEBUG
 
 /// Check for equality (==)
-#define DCHECK_EQ(x, y) DCHECK_((x) == (y), x,y)
+#define DCHECK_EQ(x, y) DCHECK_BINARY((x) == (y), x, y)
 /// Check for inequality (!=)
-#define DCHECK_NE(x, y) DCHECK_((x) != (y), x,y)
+#define DCHECK_NE(x, y) DCHECK_BINARY((x) != (y), x, y)
 /// Check for less-than or equal (<=)
-#define DCHECK_LE(x, y) DCHECK_((x) <= (y), x,y)
+#define DCHECK_LE(x, y) DCHECK_BINARY((x) <= (y), x, y)
 /// Check for less-than (<)
-#define DCHECK_LT(x, y) DCHECK_((x) < (y) ,x,y)
+#define DCHECK_LT(x, y) DCHECK_BINARY((x) <  (y), x, y)
 /// Check for greater-than or equal (>=)
-#define DCHECK_GE(x, y) DCHECK_((x) >= (y),x,y )
+#define DCHECK_GE(x, y) DCHECK_BINARY((x) >= (y), x, y)
 /// Check for greater-than (<)
-#define DCHECK_GT(x, y) DCHECK_((x) > (y) ,x,y)
+#define DCHECK_GT(x, y) DCHECK_BINARY((x) >  (y), x, y)
 
 }
