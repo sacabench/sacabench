d43a3571db48becafcd292e99adeb21d5487e0f1
Marvin Löbel <loebel.marvin@gmail.com>
2018-05-01 10:29:28 +0200

    Documented and upgraded implementation a bit

--- sacabench/util/assertions.hpp
+++ sacabench/util/assertions.hpp
@@ -1,18 +1,47 @@
 #pragma once
 
+#include <stringstream>
+
 namespace sacabench::util {
 
+#ifdef DEBUG
 #ifndef DCHECK
-#define DCHECK_(x,y,z) \
-  if (!(x)) throw std::runtime_error(std::string(" in file ") + __FILE__ + ':' + std::to_string(__LINE__) + (" the check failed: " #x) + ", we got " + std::to_string(y) + " vs " + std::to_string(z))
-#define DCHECK(x) \
-  if (!(x)) throw std::runtime_error(std::string(" in file ") + __FILE__ + ':' + std::to_string(__LINE__) + (" the check failed: " #x))
-#define DCHECK_EQ(x, y) DCHECK_((x) == (y), x,y)
-#define DCHECK_NE(x, y) DCHECK_((x) != (y), x,y)
-#define DCHECK_LE(x, y) DCHECK_((x) <= (y), x,y)
-#define DCHECK_LT(x, y) DCHECK_((x) < (y) ,x,y)
-#define DCHECK_GE(x, y) DCHECK_((x) >= (y),x,y )
-#define DCHECK_GT(x, y) DCHECK_((x) > (y) ,x,y)
+/// Internal macro for checking a boolean value
+/// and printing two comparison values.
+#define DCHECK_(x,y,z)                                           \
+    if (!(x)) {                                                  \
+        std::sstream msg;                                        \
+        msg << " in file " << __FILE__ << ":" << __LINE__;       \
+        msg << ("the check failed: " #x) ;                       \
+        msg << ", we got " << y << " vs " << z;                  \
+        throw std::runtime_error(msg.str());                     \
+    }
+/// Macro for checking a boolean value.
+#define DCHECK(x)                                                \
+    if (!(x)) {                                                  \
+        std::sstream msg;                                        \
+        msg << " in file " << __FILE__ << ":" << __LINE__;       \
+        msg << ("the check failed: " #x) ;                       \
+        throw std::runtime_error(msg.str());                     \
+    }
 #endif //DCHECK
+#else //DEBUG
+// Define macros as empty
+#define DCHECK_(x,y,z)
+#define DCHECK(x)
+#endif //DEBUG
+
+/// Check for equality (==)
+#define DCHECK_EQ(x, y) DCHECK_((x) == (y), x,y)
+/// Check for inequality (!=)
+#define DCHECK_NE(x, y) DCHECK_((x) != (y), x,y)
+/// Check for less-than or equal (<=)
+#define DCHECK_LE(x, y) DCHECK_((x) <= (y), x,y)
+/// Check for less-than (<)
+#define DCHECK_LT(x, y) DCHECK_((x) < (y) ,x,y)
+/// Check for greater-than or equal (>=)
+#define DCHECK_GE(x, y) DCHECK_((x) >= (y),x,y )
+/// Check for greater-than (<)
+#define DCHECK_GT(x, y) DCHECK_((x) > (y) ,x,y)
 
 }
