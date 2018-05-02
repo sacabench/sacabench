57ad271b60a1ab9593ee8ce3f1198950126d900c
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2018-05-02 17:14:45 +0200

    Added tests for multikey-quicksort and finalized multikey-quicksort

--- test/test_multikey_quicksort.cpp
+++ test/test_multikey_quicksort.cpp
@@ -4,20 +4,31 @@
  * All rights reserved. Published under the BSD-3 license in the LICENSE file.
  ******************************************************************************/
 
+#include <iostream>
 #include <gtest/gtest.h>
 #include <util/is_sorted.hpp>
 #include <util/sort/multikey_quicksort.hpp>
 #include <util/string.hpp>
+//#include <util/sa_check.hpp>
 
 TEST(multikey_quicksort, simple_test) {
     using namespace sacabench::util;
 
-    string_span input = "hallo"_s;
-    std::vector<size_t> array = {0, 1, 2, 3, 4};
+    const string_span input = "caabaccaabacaa"_s;
+
+    std::vector<size_t> array;
+    for(size_t i = 0; i < input.size(); ++i) {
+        array.push_back(i);
+    }
 
     sort::multikey_quicksort::multikey_quicksort(span(array), input);
 
-    auto cmp = sort::multikey_quicksort::generate_multikey_key_function(input);
+    auto cmp = sort::multikey_quicksort::generate_multikey_key_function<size_t>(input);
 
-    ASSERT_TRUE(is_sorted(array, cmp));
+    for(size_t i = 0; i < array.size(); ++i) {
+        std::cout << array[i] << std::endl;
+    }
+
+    ASSERT_TRUE(is_sorted(span(array), cmp));
+    //ASSERT_TRUE(sa_check(span(array), input));
 }
