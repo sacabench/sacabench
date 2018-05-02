d1c854deb28452d4617c7b808924c942904a7d94
magiera <oliver.magiera@tu-dortmund.de>
2018-05-02 16:52:47 +0200

    Adapted compare function to group decision. Lambda for readability.

--- sacabench/util/insertionsort.hpp
+++ sacabench/util/insertionsort.hpp
@@ -15,15 +15,18 @@ namespace sacabench::util{
 struct greater_than {
   template<typename T>
   bool operator()(T const& a, T const& b) {
-    return a > b;
+    return a - b;
   }
 };
 
   template<typename T, typename F = greater_than> void insertion_sort(span<T> A, F compare_fun = F()){
-    for(size_t i = 1; i < A.size(); i++) {
+      //Adapter for "a > b"
+      auto greater = [](auto a, auto b) { return compare_fun(a, b) > 0; };
+
+      for(size_t i = 1; i < A.size(); i++) {
       auto to_sort = A[i];
       auto j = i;
-      while((j > 0) && compare_fun(A[j-1], to_sort)) {
+      while((j > 0) && greater(A[j-1], to_sort)) {
         A[j] = A[j - 1];
         j = j - 1;
       }
