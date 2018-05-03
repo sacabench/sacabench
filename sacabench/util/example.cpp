50e2884ba3466614183aa866f9eab2c4478dc10e
Johannes Bahne <johannes.bahne@tu-dortmund.de>
2018-05-03 15:19:23 +0200

    changed input variables and comment out the assert

--- sacabench/util/example.cpp
+++ sacabench/util/example.cpp
@@ -8,11 +8,12 @@ int main() {
     std::cout << "Running example1" << std::endl;
 
     std::string input_string = "caabaccaabacaa$";
+    std::string t_0 = "cbcba"; //positions i mod 3 = 0 of input_string
     std::array<uint32_t, 10> isa = { 4,8,3,7,2,6,10,5,9,1 };
     std::array<uint32_t, 5> sa0;
 
-    sacabench::util::induce_sa_dc<char>(input_string, isa, sa0);
-
+    sacabench::util::induce_sa_dc<char>(t_0, isa, sa0);
+    
     std::cout << "Finished example1" << std::endl;
 }
 
--- sacabench/util/induce_sa_dc.hpp
+++ sacabench/util/induce_sa_dc.hpp
@@ -12,31 +12,34 @@
 #pragma once
 namespace sacabench::util {
     template<typename C, typename T, typename I, typename S>
-    //template T for input string
     //template C for input characters
+    //template T for input string
     //template I for ISA
     //template S for SA
 
     /**\brief Identify order of chars starting in position i mod 3 = 0 with difference cover
-    * \param input input text
-    * \param isa12 calculated ISA for triplets beginning in i mod 3 != 0
-    * \param sa0 memory block for resulting SA for positions beginning in i mod 3 = 0
+    * \param t_0 input text t_0 with chars beginning in i mod 3 = 0 of input text
+    * \param isa_12 calculated ISA for triplets beginning in i mod 3 != 0
+    * \param sa_0 memory block for resulting SA for positions beginning in i mod 3 = 0
     *
     * This method identifies the order of the characters in input_string in positions i mod 3 = 0
     * with information of ranks of triplets starting in position i mod 3 != 0 of input string.
     * This method works correct because of the difference cover idea.
     */
-    void induce_sa_dc(T& input, I& isa_12, S& sa_0) {
+    void induce_sa_dc(T& t_0, I& isa_12, S& sa_0) {
 
-        static_assert(sa_0.size() > 0, "Suffix Array has to be initialised.");
+        //static_assert(sa_0.size() == t_0.size(), "sa_0 must be initialised and must have the same length as t_0.");
+        //TODO: Use DCHECK() 
 
-        std::array<std::tuple<C, int>, sa_0.size()> sa0_to_be_sorted;
+        std::array<std::tuple<C, int>, sa_0.size()> sa_0_to_be_sorted;
 
         for (int i = 0; i < sa_0.size(); i++) {
-            sa0_to_be_sorted[i] = std::tuple<C, int>(input[3 * i], isa_12[i]);
+            sa_0_to_be_sorted[i] = std::tuple<C, int>(t_0[i], isa_12[i]);
         }  
     
         //TODO: sort Tupels with radix_sort
         //radix_sort(sa0_to_be_sorted, sa0);
+        
+        
     }
 }  // namespace sacabench::util
