d9e19ad6b5edc55d858af6820dda6a41f5a0611d
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2018-05-03 20:53:38 +0200

    Fixed multikey quicksort

--- sacabench/util/sort/multikey_quicksort.hpp
+++ sacabench/util/sort/multikey_quicksort.hpp
@@ -7,18 +7,27 @@
 
 #pragma once
 
+// #include <iostream>
 #include <cinttypes>
 #include <util/sort/ternary_quicksort.hpp>
 #include <util/span.hpp>
 #include <util/string.hpp>
+#include <util/sort/std_sort.hpp>
+
+void print_suffix(const sacabench::util::string_span& input_text, size_t start) {
+    // for(size_t i = start; i < input_text.size(); ++i) {
+    //     std::cout << (size_t) input_text[i] << " ";
+    // }
+    // std::cout << std::endl;
+}
 
 namespace sacabench::util::sort::multikey_quicksort {
 // Create a function with compares at one character depth.
 template <typename index_type>
 struct compare_one_character_at_depth {
 public:
-    compare_one_character_at_depth(const string_span& input_text)
-        : depth(0), input_text(input_text) {}
+    compare_one_character_at_depth(const string_span& _input_text)
+        : depth(0), input_text(_input_text) {}
 
     // The depth at which we compare.
     index_type depth;
@@ -26,18 +35,32 @@ public:
     // 0 if equal, < 0 if the first is smaller, > 0 if the first is larger.
     // Overwrites -> operator for quicksort
     int operator()(const index_type& a, const index_type& b) const {
-        int diff = this->input_text[depth + a] - this->input_text[depth + b];
+        if(this->depth + a >= this->input_text.size()) {
+            // a should be larger
+            return -1;
+        }
+
+        if(this->depth + b >= this->input_text.size()) {
+            // a should be larger
+            return 1;
+        }
+
+        size_t at_a = this->input_text.at(depth + a);
+        size_t at_b = this->input_text.at(depth + b);
+
+        int diff = at_a - at_b;
+
         return diff;
     }
 
-private:
+//private: FIXME
     // A reference to the input text.
-    const string_span& input_text;
+    const string_span input_text;
 };
 
 // Create a new key_func to compare two characters at depth 0.
 template <typename index_type>
-inline compare_one_character_at_depth<index_type>
+compare_one_character_at_depth<index_type>
 generate_multikey_key_function(const string_span input_text) {
     compare_one_character_at_depth<index_type> obj(input_text);
     return obj;
@@ -45,7 +68,7 @@ generate_multikey_key_function(const string_span input_text) {
 
 // Our internal sort function.
 template <typename index_type>
-inline void multikey_quicksort_internal(
+void multikey_quicksort_internal(
     span<index_type> array,
     compare_one_character_at_depth<index_type>& key_func) {
     // If the set size is only one element, we don't need to sort.
@@ -56,17 +79,37 @@ inline void multikey_quicksort_internal(
     // FIXME: Choose a simple pivot element.
     const index_type& pivot_element = array[0];
 
+    // std::cout << "1---" << std::endl;
+    // for(size_t i = 0; i < array.size(); ++i) {
+    //     print_suffix(key_func.input_text, array[i]);
+    // }
+    // std::cout << "1---" << std::endl;
+
+    // FIXME: This is a work around, because the partitioning by ternary doesn't work correctly!
+    auto less = [key_func](const index_type& a, const index_type& b) { return(key_func(a,b) < 0); };
+    sort::std_sort(array, less);
+    // FIXME: End of workaround
+
     // Swap elements using ternary quicksort partitioning.
     // Casts key_func into type std::function<int(index_type, index_type)>
     auto bounds = sort::ternary_quicksort::partition(array, key_func,
-                                                             pivot_element);
+                                                           pivot_element);
+
+    // std::cout << "Sorted until depth " <<  key_func.depth << std::endl;
+    // std::cout << "2---" << std::endl;
+    // for(size_t i = 0; i < array.size(); ++i) {
+    //     print_suffix(key_func.input_text, array[i]);
+    // }
+    // std::cout << "2---" << std::endl;
+
+    //assert(is_sorted(array, key_func));
 
     // Invariant: 0 .. bounds[0] is lesser than pivot_element
     // bounds[0] .. bounds[1] is equal to the pivot_element
     // bounds[1] .. n is greater than the pivot_element
     auto lesser = array.slice(0, bounds.first);
     auto equal = array.slice(bounds.first, bounds.second);
-    auto greater = array.slice(bounds.second);
+    auto greater = array.slice(bounds.second, array.size());
 
     // Recursively sort the lesser and greater partitions by the same depth.
     multikey_quicksort_internal(lesser, key_func);
@@ -81,8 +124,11 @@ inline void multikey_quicksort_internal(
 // Sort the suffix indices in array by comparing one character in
 // input_text.
 template <typename index_type>
-inline void multikey_quicksort(span<index_type> array,
+void multikey_quicksort(span<index_type> array,
                                const string_span input_text) {
+
+    // print_suffix(input_text, 0);
+
     // Generate key function which compares only the character at position 0.
     auto key_func = generate_multikey_key_function<index_type>(input_text);
 
--- test/test_multikey_quicksort.cpp
+++ test/test_multikey_quicksort.cpp
@@ -4,14 +4,55 @@
  * All rights reserved. Published under the BSD-3 license in the LICENSE file.
  ******************************************************************************/
 
+#include <util/assertions.hpp>
 #include <iostream>
 #include <gtest/gtest.h>
 #include <util/is_sorted.hpp>
 #include <util/sort/multikey_quicksort.hpp>
 #include <util/string.hpp>
-//#include <util/sa_check.hpp>
+#include <util/sa_check.hpp>
 
-TEST(multikey_quicksort, simple_test) {
+TEST(multikey_quicksort, abc) {
+    using namespace sacabench::util;
+
+    const string_span input = "abc"_s;
+
+    std::vector<size_t> array;
+    for(size_t i = 0; i < input.size(); ++i) {
+        array.push_back(i);
+    }
+
+    ASSERT_EQ(array.size(), input.size());
+
+    sort::multikey_quicksort::multikey_quicksort(span(array), input);
+
+    for(size_t i = 0; i < array.size(); ++i) {
+        std::cout << array[i] << std::endl;
+    }
+
+    ASSERT_TRUE(sa_check(span(array), input));
+}
+
+TEST(multikey_quicksort, ba) {
+    using namespace sacabench::util;
+
+    const string_span input = "ba"_s;
+
+    std::vector<size_t> array;
+    for(size_t i = 0; i < input.size(); ++i) {
+        array.push_back(i);
+    }
+
+    sort::multikey_quicksort::multikey_quicksort(span(array), input);
+
+    for(size_t i = 0; i < array.size(); ++i) {
+        std::cout << array[i] << std::endl;
+    }
+
+    ASSERT_TRUE(sa_check(span(array), input));
+}
+
+TEST(multikey_quicksort, caabaccaabacaa) {
     using namespace sacabench::util;
 
     const string_span input = "caabaccaabacaa"_s;
@@ -23,12 +64,9 @@ TEST(multikey_quicksort, simple_test) {
 
     sort::multikey_quicksort::multikey_quicksort(span(array), input);
 
-    auto cmp = sort::multikey_quicksort::generate_multikey_key_function<size_t>(input);
-
     for(size_t i = 0; i < array.size(); ++i) {
         std::cout << array[i] << std::endl;
     }
 
-    ASSERT_TRUE(is_sorted(span(array), cmp));
-    //ASSERT_TRUE(sa_check(span(array), input));
+    ASSERT_TRUE(sa_check(span(array), input));
 }
