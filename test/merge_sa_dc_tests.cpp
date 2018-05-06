cc9459e5294b351e27f8bc62963893c67e919bd2
NicoBertram <nico.bertram@tu-dortmund.de>
2018-05-06 22:51:15 +0200

    Implemented tests

---
+++ test/merge_sa_dc_tests.cpp
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (C) 2018 Nico Bertram <nico.bertram@tu-dortmund.de>
+ *
+ * All rights reserved. Published under the BSD-3 license in the LICENSE file.
+ ******************************************************************************/
+
+#include <gtest/gtest.h>
+#include <util/merge_sa_dc.hpp>
+#include <util/string.hpp>
+#include <util/container.hpp>
+
+
+TEST(DC, induce) {    
+    sacabench::util::string input_string = {'c', 'a', 'a', 'b', 'a', 'c', 'c',
+            'a', 'a', 'b', 'a', 'c', 'a', 'a'};
+    
+    //TODO
+    auto sa_0 = sacabench::util::container<size_t> { 12,9,3,6,0 };
+    auto sa_12 = sacabench::util::container<size_t> { 14,13,7,1,8,2,10,4,11,5 };
+    auto isa_12 = sacabench::util::container<size_t> { 0,3,5,0,7,9,0,2,4,0,6,8,0,1,0 };
+    
+    //empty SA which should be filled correctly with method merge_sa_dc
+    auto sa = sacabench::util::container<size_t> {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    
+    //implementation of get-substring method
+    template <typename T>
+    span<t> get_substring(T& t, int pos, int n) {
+        return t.subspan(pos, n);
+    }
+    
+    // implementation of comp method
+    bool comp(sacabench::util::string a, sacabench::util::string b) {
+        return a < b;
+    }
+    
+    //run method to test it
+    sacabench::util::merge_sa_dc<unsigned char>(input_string, sa_0, sa_12, isa_12, sa, comp);
+
+    //expected values for induced SA with DC
+    auto expected = sacabench::util::container<size_t> {14,13,12,7,1,8,2,10,4,9,3,11,6,0,5};
+    
+    //compare results with expected values
+    for (int i = 0; i < expected.size(); i++) {
+        ASSERT_EQ(sa_0[i], expected[i]);
+    }
+}
