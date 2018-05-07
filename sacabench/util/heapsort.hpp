f41c1e0b0d1bcb7de2ad56818113252608153fee
magiera <oliver.magiera@tu-dortmund.de>
2018-05-07 21:48:35 +0200

    Fixed loop start point for build_max_heap. Fixed indexing of children. Fixed wrong heap_size in heapsort for restoring max-heap condition.

--- sacabench/util/heapsort.hpp
+++ sacabench/util/heapsort.hpp
@@ -23,14 +23,14 @@ namespace sacabench::util {
      * Returns the position of the left child of a node in a heap.
      */
     size_t child_left(size_t node) {
-        return 2 * node;
+        return 2 * (node+1) - 1;
     }
 
     /*
      * Returns the position of the right child of a node in a heap.
      */
     size_t child_right(size_t node) {
-        return 2 * node + 1;
+        return 2 * (node + 1);
     }
 
 
@@ -47,25 +47,26 @@ namespace sacabench::util {
         auto left = child_left(node);
         auto right = child_right(node);
 
-        size_t max_value;
+        size_t max_value_index;
         //Check wether left child is greater than it's parent
-        left < heap_size && greater(data[left], data[node]) ? max_value = left
-                                                     : max_value = node;
+        left < heap_size && greater(data[left], data[node]) ?
+                max_value_index = left : max_value_index = node;
 
         //Check wether right child is greater than current max value
         if (right < heap_size &&
-            greater(data[right], data[node])) { max_value = right; }
+            greater(data[right], data[max_value_index])) { max_value_index = right; }
 
         // Swap elements if swap is needed
-        if (max_value != node) {
-            std::swap(data[node], data[max_value]);
-            max_heapify(data, heap_size, max_value);
+        if (max_value_index != node) {
+            std::swap(data[node], data[max_value_index]);
+            max_heapify(data, heap_size, max_value_index);
         }
     }
 
     template<typename T> void build_max_heap(span<T> data) {
         // +1 to work around size_t being unsigned
-        for (size_t i = std::floor(data.size() - 1 / 2) + 1; i != 0; --i) {
+        for (size_t i = std::floor(data.size() / 2); i != 0; --i) {
+
             max_heapify(data, data.size(), i - 1);
         }
     }
@@ -79,11 +80,11 @@ namespace sacabench::util {
         build_max_heap(data);
         // Invariant: data[0...heap_size) unsorted,
         // data[heap_size, data.size()) sorted
-        for (size_t heap_size = data.size(); heap_size != 0; --heap_size) {
+        for (size_t heap_size = data.size(); heap_size != 1; --heap_size) {
             //Swap first (biggest) and last element in heap
             std::swap(data[0], data[heap_size - 1]);
-            //Restore max-heap property
-            max_heapify(data, heap_size, 0);
+            // Restore max-heap property for heap with heap_size - 1
+            max_heapify(data, heap_size - 1, 0);
         }
     }
 }
