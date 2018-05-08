e43899e35204128f7745d4913df6ac9a79693c69
magiera <oliver.magiera@tu-dortmund.de>
2018-05-08 15:09:10 +0200

    Fixed recursive calls to not contain the main comp_func. Added comp_func to build_heap and heapsort.

--- sacabench/util/heapsort.hpp
+++ sacabench/util/heapsort.hpp
@@ -13,6 +13,10 @@
 
 namespace sacabench::util {
 
+    /*
+     * struct containing the default comparison function.
+     * TODO: replace with
+     */
     struct greater_than {
         template<typename T>
         int64_t operator()(T const& a, T const& b) {
@@ -21,6 +25,8 @@ namespace sacabench::util {
     };
     /*
      * Returns the position of the left child of a node in a heap.
+     *
+     * node: The parent to retrieve the child from.
      */
     size_t child_left(size_t node) {
         return 2 * (node+1) - 1;
@@ -28,6 +34,8 @@ namespace sacabench::util {
 
     /*
      * Returns the position of the right child of a node in a heap.
+     *
+     * node: The parent to retrieve the child from.
      */
     size_t child_right(size_t node) {
         return 2 * (node + 1);
@@ -36,7 +44,11 @@ namespace sacabench::util {
 
     /*
      * Applies the max-heap condition on a given node.
-     * FIXME: Not sorting correctly (max-heap condition not correctly executed)
+     *
+     * data: The data to work on.
+     * heap_size: The current size of the heap (not necessarily same with
+     * data.size())
+     * compare_fun: The comparison function to compare elements with.
      */
     template<typename T, typename F=greater_than> void max_heapify
             (span<T> data, size_t heap_size, size_t node, F compare_fun=F()) {
@@ -47,27 +59,30 @@ namespace sacabench::util {
         auto left = child_left(node);
         auto right = child_right(node);
 
-        size_t max_value_index;
         //Check wether left child is greater than it's parent
-        left < heap_size && greater(data[left], data[node]) ?
-                max_value_index = left : max_value_index = node;
-
+        size_t max_value_index = left < heap_size &&
+                                 greater(data[left], data[node]) ? left : node;
         //Check wether right child is greater than current max value
-        if (right < heap_size &&
-            greater(data[right], data[max_value_index])) { max_value_index = right; }
+        if (right < heap_size && greater(data[right], data[max_value_index]))
+            { max_value_index = right; }
 
         // Swap elements if swap is needed
         if (max_value_index != node) {
             std::swap(data[node], data[max_value_index]);
-            max_heapify(data, heap_size, max_value_index);
+            max_heapify(data, heap_size, max_value_index, compare_fun);
         }
     }
 
-    template<typename T> void build_max_heap(span<T> data) {
+    /*
+     * Build a heap for a given span, i.e. ensure the max-heap condition for
+     * all inner nodes.
+     */
+    template<typename T, typename F=greater_than> void
+        build_max_heap(span<T> data, F compare_fun=F()) {
         // +1 to work around size_t being unsigned
         for (size_t i = std::floor(data.size() / 2); i != 0; --i) {
-
-            max_heapify(data, data.size(), i - 1);
+            // Call max_heapify for all non-leaves
+            max_heapify(data, data.size(), i - 1, compare_fun);
         }
     }
 
@@ -75,8 +90,10 @@ namespace sacabench::util {
      * Sort an input according to heapsort in ascending order.
      *
      * data: The input data to be sorted.
+     * compare_fun: The comparison function to be used.
      */
-    template<typename T> void heapsort(span<T> data) {
+    template<typename T, typename F=greater_than> void
+        heapsort(span<T> data, F compare_fun=F()) {
         build_max_heap(data);
         // Invariant: data[0...heap_size) unsorted,
         // data[heap_size, data.size()) sorted
@@ -84,7 +101,7 @@ namespace sacabench::util {
             //Swap first (biggest) and last element in heap
             std::swap(data[0], data[heap_size - 1]);
             // Restore max-heap property for heap with heap_size - 1
-            max_heapify(data, heap_size - 1, 0);
+            max_heapify(data, heap_size - 1, 0, compare_fun);
         }
     }
 }
