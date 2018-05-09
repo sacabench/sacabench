b49ea305ed927561dc10383ad892971f595c8a17
jonas.bode@tu-dortmund.de <jonas.bode@tu-dortmund.de>
2018-05-09 17:31:58 +0200

    Uploaded a newer and more optimized version of type_extraction.hpp

--- sacabench/util/type_extraction.hpp
+++ sacabench/util/type_extraction.hpp
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2018 Janina Michaelis <janina.michaelis@tu-dortmund.de>
  * Copyright (C) 2018 Jonas Bode <jonas.bode@tu-dortmund.de>
- * 
+ *
  * All rights reserved. Published under the BSD-3 license in the LICENSE file.
  ******************************************************************************/
 #include <array>
@@ -14,41 +14,45 @@
 
 namespace sacabench::util {
 
-	/** Check if c_1 comes after c_2 in the given alphabet
-	*/
-	bool is_larger_char(char c_1, char c_2, string_span alph) {
+    /** Check if c_1 comes after c_2 in the given alphabet
+    */
+    size_t get_alphabet_index(char c, string_span alph) {
 
-		if (c_1 == c_2) {
-			return false;
-		}
+        for (size_t i = 0; i < alph.size(); i++) {
+            if (alph[i] == c) {
+                return i;
+            }
+        }
+    }
 
-		for (int i = 0; i < alph.size(); i++) {
-			if (alph[i] == c_1) {
-				return true;
-			}
-			if (alph[i] == c_2) {
-				return false;
-			}
-		}
-	}
+    /**\Save the L/S-Types of the input text in a given array (boolean: 1 = L, 0 = S)
+    * \param t_0 input text
+    * \param alph alphabet
+    * \param ba boolean array where the result is saved
+    */
+    void get_types(string_span t_0, string_span alph, span<bool> ba) {
 
-	/**\Save the L/S-Types of the input text in a given array (boolean: 1 = L, 0 = S)
-	* \param t_0 input text
-	* \param alph alphabet
-	* \param ba boolean array where the result is saved
-	*/
-	void get_types(string_span t_0, string_span alph, span<bool> ba) {
+        DCHECK_MSG(t_0.size() == ba.size(), "t_0 must have the same length as the type array ba");
 
-		DCHECK_MSG(t_0.size() == ba.size(), "t_0 must have the same length as the type array ba");
+        size_t last_alphabet_index = 0;
 
-		for (int i = t_0.size() - 1; i >= 0; i--) {
-			ba[i] = (i != t_0.size() - 1 && 					// case 1: Symbol is sentinel
-				(is_larger_char(t_0[i], t_0[i + 1], alph) || 	// case 2: Symbol is larger then following Symbol
-				(t_0[i] == t_0[i + 1] && ba[i + 1] == true)));	// case 3: Symbol is equal to following Symbol
-		}
+        // For size_t we need to shift everything by -1 (because size_t always is >= 0)
 
-	}
+        for (size_t j = t_0.size(); j > 0; j--) {
 
+            size_t i = j - 1;
+
+            bool const not_sentinel = (i != t_0.size() - 1);
+            size_t const current_alphabet_index = get_alphabet_index(t_0[i], alph);
+
+            ba[i] = (not_sentinel &&		                                    // Symbol is not sentinel
+                (current_alphabet_index > last_alphabet_index || 	            // Symbol is larger than following Symbol OR
+                (current_alphabet_index == last_alphabet_index && ba[i + 1])));	// Symbol is equal to following Symbol
+
+            last_alphabet_index = current_alphabet_index;
+        }
+
+    }
 }
 
 /******************************************************************************/
