aaa83fcf635faa6b5d8e3b09f8dff8479a74fe5d
magiera <oliver.magiera@tu-dortmund.de>
2018-05-09 14:21:26 +0200

    Added doxygen comments to introsort.hpp. Added introsort_tests.cpp.

--- sacabench/util/introsort.hpp
+++ sacabench/util/introsort.hpp
@@ -13,27 +13,41 @@
 #include "insertionsort.hpp"
 #include "sort/ternary_quicksort.hpp"
 
-//TODO: Add test for introsort
 //TODO: Add quicksort to base
+/** \file introsort.hpp
+ * \brief Implements the introspective sort sorting method. (see Introspective
+ * Sorting and Selection Algorithms, David R. Musser)
+ */
+
 namespace sacabench::util {
 
     // Change threshold to empirically relevant value (e.g. 8 or 15)
     // Value of 2 for testing
-    const size_t SIZE_THRESHOLD = 2;
+    const size_t SIZE_THRESHOLD = 2; /*< \brief Defines the threshold for using
+    insertionsort instead of quicksort. */
 
-    /*
-     * Sorts the given span in increasing order according to the given
-     * comparison function
+    /**
+     * \brief Sorts the given span in increasing order according to the given
+     * comparison function.
      *
-     * data: The span containing all elements to be sorted.
-     * compare_fun: The comparison function to be used.
+     * Sorts the submitted data via Introspective Sort (see Introspective
+     * Sorting and Selection Algorithms, David R. Musser). It partitions the
+     * data to be sorted in the ternary quicksort fashion using median of 3 as
+     * pivot element. After a depth of floor(lg(|data|)) heapsort is used
+     * instead of quicksort. At any time when |data| < SIZE_THRESHOLD, the final
+     * interval is sorted by insertionsort.
+     *
+     * @tparam T The data type to be sorted. Needs to be comparable.
+     * @tparam F The used compare function. Uses std::less<T> by default.
+     * @param[in,out] data The span containing all elements to be sorted.
+     * @param[in] compare_fun The comparison function of type F to be used for
+     * comparing two elements
      */
-    template<typename T, typename F=std::less<int>>
+    template<typename T, typename F=std::less<T>>
     void introsort
             (span<T> data, F compare_fun = F()) {
         // Max. number of allowed iterations: 2*log(size)
-        // due to empirically good results (see Introspective Sorting and
-        // Selection Algorithms, David R. Musser)
+        // due to empirically good results
         size_t max_iterations = 2 * floor(log(last_index - first_index));
         introsort_internal(data, max_iterations, compare_fun);
 
@@ -42,9 +56,26 @@ namespace sacabench::util {
         insertion_sort(data, compare_fun);
     }
 
-    template<typename T, typename F=std::less<int>>
+    /**
+     * \brief The internal loop of introsort.
+     *
+     * Executes the main logic of introsort. While the max. depth is not
+     * reached, it partitions the span according to ternary quicksort, otherwise
+     * heapsort is used. If, at any time, the size of the span to be sorted is
+     * smaller than the threshold, then insertionsort is used.
+     *
+     * @tparam T The data type to be sorted. Needs to be comparable.
+     * @tparam F The comparable object. std::less<T> by default.
+     * @param data The span containing all elements to be sorted.
+     * @param depth_limit The maximum number of iterations, after heapsort is
+     * used.
+     * @param compare_fun The comparison function of type F to be used for
+     * comparing two elements in data.
+     */
+    template<typename T, typename F=std::less<T>>
     void introsort_internal(span<T> data, size_t depth_limit,
                             F compare_fun = F()) {
+        // "break"-condition for using insertionsort (data is small enough)
         while(data.size() > SIZE_THRESHOLD) {
             // Use heapsort to finish sorting, end loop.
             if(depth_limit == 0) {
@@ -61,14 +92,14 @@ namespace sacabench::util {
                 auto bounds = sort::ternary_quicksort::partition
                          (data, compare_fun, pivot);
 
-                // Create partition with partitioning bounds
+                // Create partitions with partitioning bounds
                 span<T> lesser = data.slice(0, bounds[0]);
                 span<T> greater = data.slice(bounds[1], data.size());
 
                 // Recursive call only on interval with elements greater pivot
                 introsort_internal(greater, depth_limit, compare_fun);
 
-                // Overwrite data with lesser (while loop continues with lesser)
+                // Overwrite data with lesser (while-loop continues with lesser)
                 data = lesser;
             }
         }
---
+++ test/introsort_tests.cpp
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * test/introsort_tests.cpp
+ *
+ * Copyright (C) 2018 Rosa Pink
+ *
+ * All rights reserved. Published under the BSD-3 license in the LICENSE file.
+ ******************************************************************************/
+
+#include <gtest/gtest.h>
+#include <util/introsort.hpp>
+#include <util/container.hpp>
+
+using namespace sacabench::util;
+
+TEST(InSort, sort_correct) {
+    container<int> test_inst = {3,1,9,6,4,2,7,8,5};
+    container<int> control_inst = {1,2,3,4,5,6,7,8,9};
+    //heapsort(span(test_inst));
+    introsort<int>(test_inst);
+    ASSERT_EQ(test_inst, control_inst);
+}
