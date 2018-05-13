9662c08665c2c9e8ad26e61bf6b55c9604a4b723
Johannes Bahne <johannes.bahne@tu-dortmund.de>
2018-05-13 12:33:27 +0200

    finished determine triplets including tests

--- sacabench/saca/dc3.hpp
+++ sacabench/saca/dc3.hpp
@@ -11,6 +11,7 @@
 #include <util/string.hpp>
 #include <vector>
 #include <util/container.hpp>
+#include <tuple>
 
 namespace sacabench::saca {
     
@@ -24,51 +25,51 @@ namespace sacabench::saca {
     * This method works correct because of the difference cover idea.
     */
     
-    template<typename C, typename T>
-    void determine_triplets(const T& INPUT_STRING) {
+    template<typename C, typename T, typename S>
+    void determine_triplets(const T& INPUT_STRING, S& sa_12) {
         
-        const sacabench::util::string SMALLEST_CHAR = {' '};
+        //const sacabench::util::string SMALLEST_CHAR = {' '};
+        const unsigned char SMALLEST_CHAR = ' ';
         //Container to store all tuples with the same length as sa_0
         //Tuples contains a char and a rank 
         auto sa_12_to_be_sorted = sacabench::util::make_container<std::tuple<C, C, C, size_t>>(2*INPUT_STRING.size()/3);
         
-        for (size_t i = 1; i < INPUT_STRING.size(); i++) {
+        size_t counter = 0;
+        for(size_t i = 1; i < INPUT_STRING.size(); i++) {
             if(i % 3 == 1){
                 if((i+2) >= INPUT_STRING.size()){
                     if((i+1) >= INPUT_STRING.size()){
-                        sa_12_to_be_sorted[i-1] = std::tuple<C, C, C, size_t>(INPUT_STRING[i], SMALLEST_CHAR, SMALLEST_CHAR, 3 * i - 2);
+                        sa_12_to_be_sorted[counter++] = std::tuple<C, C, C, size_t>(INPUT_STRING[i], SMALLEST_CHAR, SMALLEST_CHAR, i);
                     }else{
-                        sa_12_to_be_sorted[i-1] = std::tuple<C, C, C, size_t>(INPUT_STRING[i], INPUT_STRING[i+1], SMALLEST_CHAR, 3 * i - 2);
+                        sa_12_to_be_sorted[counter++] = std::tuple<C, C, C, size_t>(INPUT_STRING[i], INPUT_STRING[i+1], SMALLEST_CHAR, i);
                     }
                 }else{
-                    sa_12_to_be_sorted[i-1] = std::tuple<C, C, C, size_t>(INPUT_STRING[i], INPUT_STRING[i+1], INPUT_STRING[i+2], 3 * i -2);
+                    sa_12_to_be_sorted[counter++] = std::tuple<C, C, C, size_t>(INPUT_STRING[i], INPUT_STRING[i+1], INPUT_STRING[i+2], i);
                 }
                 
             }
             else if(i % 3 == 2){
                 if((i+2) >= INPUT_STRING.size()){
                     if((i+1) >= INPUT_STRING.size()){
-                        sa_12_to_be_sorted[i-1] = std::tuple<C, C, C, size_t>(INPUT_STRING[i], SMALLEST_CHAR, SMALLEST_CHAR, 3 * i - 1);
+                        sa_12_to_be_sorted[counter++] = std::tuple<C, C, C, size_t>(INPUT_STRING[i], SMALLEST_CHAR, SMALLEST_CHAR, i);
                     }else{
-                        sa_12_to_be_sorted[i-1] = std::tuple<C, C, C, size_t>(INPUT_STRING[i], INPUT_STRING[i+1], SMALLEST_CHAR, 3 * i - 1);
+                        sa_12_to_be_sorted[counter++] = std::tuple<C, C, C, size_t>(INPUT_STRING[i], INPUT_STRING[i+1], SMALLEST_CHAR, i);
 
                     }
                 }else{
-                    sa_12_to_be_sorted[i-1] = std::tuple<C, C, C, size_t>(INPUT_STRING[i], INPUT_STRING[i+1], INPUT_STRING[i+2], 3 * i -1);
+                    sa_12_to_be_sorted[counter++] = std::tuple<C, C, C, size_t>(INPUT_STRING[i], INPUT_STRING[i+1], INPUT_STRING[i+2], i);
                 }    
             }
         }  
         
-        std::vector<int> sa_12;
         //TODO: sort Tupels with radix_sort
-        //radix_sort(sa0_to_be_sorted, sa0);
-        
+        //radix_sort(sa0_to_be_sorted, sa0);        
         std::sort(sa_12_to_be_sorted.begin(),sa_12_to_be_sorted.end());
-        for(size_t i = 0; i<sa_12_to_be_sorted.size();i++){
-            sa_12.push_back(std::get<3>(sa_12_to_be_sorted[i]));
-            std::cout << sa_12[i] << std::endl;
-        }
         
+        for(size_t i = 0; i<sa_12_to_be_sorted.size();i++){   
+            sa_12[i] = std::get<3>(sa_12_to_be_sorted[i]);
+        }
+                
     }
 }  // namespace sacabench::util
 
--- test/sa_dc3_tests.cpp
+++ test/sa_dc3_tests.cpp
@@ -9,15 +9,57 @@
 #include <util/string.hpp>
 #include <util/container.hpp>
 #include <saca/dc3.hpp>
+#include <iostream>
 
 
-TEST(DC, induce) {    
-    sacabench::util::string input_string = {'c', 'a', 'a', 'b', 'a', 'c', 'c',
-            'a', 'a', 'b', 'a', 'c', 'a', 'a'};
-            
+TEST(DC3, determine_triplets) {
+    sacabench::util::string input_string = sacabench::util::make_string("caabaccaabacaa");
+    
+    //empty SA which should be filled correctly with method induce_sa_dc
+    auto sa_12 = sacabench::util::container<size_t> {0,0,0,0,0,0,0,0,0};
     
     //run method to test it
-    sacabench::saca::determine_triplets<unsigned char>(input_string);
+    sacabench::saca::determine_triplets<unsigned char>(input_string, sa_12);
     
+    
+    //expected values for induced SA with DC
+    auto expected = sacabench::util::container<size_t> {13, 1, 7, 2, 8, 10, 4, 11, 5};
+    
+    //compare results with expected values
+    ASSERT_EQ(sa_12[0], expected[0]);
+    ASSERT_EQ(sa_12[1], expected[1]);
+    ASSERT_EQ(sa_12[2], expected[2]);
+    ASSERT_EQ(sa_12[3], expected[3]);
+    ASSERT_EQ(sa_12[4], expected[4]);
+    ASSERT_EQ(sa_12[5], expected[5]);
+    ASSERT_EQ(sa_12[6], expected[6]);
+    ASSERT_EQ(sa_12[7], expected[7]);
+    ASSERT_EQ(sa_12[8], expected[8]);
+}
+
+TEST(DC3, determine_triplets_with_sentinel) {    
+    sacabench::util::string input_string = sacabench::util::make_string("caabaccaabacaa$");
+    
+    //empty SA which should be filled correctly with method induce_sa_dc
+    auto sa_12 = sacabench::util::container<size_t> {0,0,0,0,0,0,0,0,0};
+    
+    //run method to test it
+    sacabench::saca::determine_triplets<unsigned char>(input_string, sa_12);
+    
+    
+    //expected values for induced SA with DC
+    auto expected = sacabench::util::container<size_t> {14, 13, 1, 7, 2, 8, 10, 4, 11, 5};
+    
+    //compare results with expected values
+    ASSERT_EQ(sa_12[0], expected[0]);
+    ASSERT_EQ(sa_12[1], expected[1]);
+    ASSERT_EQ(sa_12[2], expected[2]);
+    ASSERT_EQ(sa_12[3], expected[3]);
+    ASSERT_EQ(sa_12[4], expected[4]);
+    ASSERT_EQ(sa_12[5], expected[5]);
+    ASSERT_EQ(sa_12[6], expected[6]);
+    ASSERT_EQ(sa_12[7], expected[7]);
+    ASSERT_EQ(sa_12[8], expected[8]);
+    ASSERT_EQ(sa_12[9], expected[9]);
 }
 
