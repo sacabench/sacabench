c680776f8632ebfb21961b5fe5e4263a1f40a067
David Piper <david.piper@udo.edu>
2018-05-16 15:31:24 +0200

    Added own test for gsaca (corner cases are still failing).

--- test/gsaca_tests.cpp
+++ test/gsaca_tests.cpp
@@ -13,4 +13,15 @@ using namespace sacabench::gsaca;
 
 TEST(gsaca, test) {
     //test::saca_corner_cases<gsaca>();
+
+    unsigned int n = 14;
+    sacabench::util::string_span text = "graindraining\0"_s;
+    sacabench::util::container<size_t> place = sacabench::util::make_container<size_t>(text.size());
+    sacabench::util::span<size_t> out_sa = sacabench::util::span<size_t>(place);
+
+    int expected_result[14] = {13, 2, 7, 5, 12, 0, 3, 10, 8, 4, 11, 9, 1, 6};
+    gsaca::construct_sa(text, 0, out_sa);
+    for (int index = 0; index < n; index++) {
+        ASSERT_EQ(out_sa[index], expected_result[index]);
+    }
 }
