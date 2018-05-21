4b7f641aa6fec97270a9bf060ec2136b7d712d8b
David Piper <david.piper@udo.edu>
2018-05-21 18:34:29 +0200

    Added custom test case for very long word.

--- test/gsaca_tests.cpp
+++ test/gsaca_tests.cpp
@@ -69,3 +69,32 @@ TEST(gsaca, test_input_4) {
         ASSERT_EQ(out_sa[index], expected_result[index]);
     }
 }
+
+TEST(gsaca, test_input_5) {
+    sacabench::util::string_span text = "asdfasctjkcbweasbebvtiwetwcnbwbbqnqxernqzezwuqwezuetqcrnzxbneqebwcbqwicbqcbtnqweqxcbwuexcbzqwezcqbwecqbwdassdasdfzdfgfsdfsdgfducezctzqwebctuiqwiiqcbnzcebzqc\0"_s;
+    sacabench::util::container<size_t> place = sacabench::util::make_container<size_t>(text.size());
+    sacabench::util::span<size_t> out_sa = sacabench::util::span<size_t>(place);
+    int expected_result[157] = {
+        156, 14, 4, 0, 109, 105, 30, 136, 16, 58,
+        147, 71, 31, 66, 74, 18, 28, 63, 102, 11,
+        97, 83, 152, 89, 155, 146, 70, 65, 73, 10,
+        82, 88, 150, 127, 26, 100, 95, 53, 6, 137,
+        130, 108, 104, 2, 114, 119, 111, 122, 125,
+        13, 135, 17, 62, 151, 99, 60, 79, 36, 50,
+        23, 86, 93, 128, 47, 41, 3, 124, 115, 117,
+        120, 112, 123, 116, 69, 143, 144, 140, 21,
+        8, 9, 27, 59, 76, 33, 38, 148, 55, 101, 96,
+        154, 145, 72, 52, 61, 32, 133, 77, 91, 45,
+        67, 141, 80, 34, 39, 37, 54, 15, 5, 107,
+        1, 118, 110, 121, 106, 20, 7, 75, 51, 138,
+        24, 131, 126, 49, 85, 139, 44, 19, 29, 64,
+        25, 103, 12, 134, 98, 78, 22, 92, 46, 68,
+        142, 84, 43, 57, 81, 87, 35, 149, 94, 129,
+        113, 40, 153, 132, 90, 48, 42, 56
+    };
+
+    gsaca::construct_sa(text, 0, out_sa);
+    for (int index = 0; index < 58; index++) {
+        ASSERT_EQ(out_sa[index], expected_result[index]);
+    }
+}
