0d23710e5f19485680e8d8bdce2735ca9a35864f
Marvin Löbel <loebel.marvin@gmail.com>
2018-05-23 10:24:35 +0200

    remove unneeded function

--- sacabench/util/stacktrace.hpp
+++ sacabench/util/stacktrace.hpp
@@ -44,20 +44,6 @@
 
 namespace sacabench::backtrace {
 
-inline int str_ends_with(const char* str, const char* suffix) {
-
-    if (str == NULL || suffix == NULL)
-        return 0;
-
-    size_t str_len = strlen(str);
-    size_t suffix_len = strlen(suffix);
-
-    if (suffix_len > str_len)
-        return 0;
-
-    return 0 == strncmp(str + str_len - suffix_len, suffix, suffix_len);
-}
-
 // This function produces a stack backtrace with demangled function & method
 // names.
 inline std::string Backtrace(int skip = 1) {
