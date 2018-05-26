cb5c476e250a1b1627ae3c447d3c59540cae1d57
@foot <hermann.foot@tu-dortmund.de>
2018-05-26 14:27:31 +0200

    Added version which only needs one additional array

--- sacabench/saca/qsufsort.hpp
+++ sacabench/saca/qsufsort.hpp
@@ -6,12 +6,17 @@
 
 #pragma once
 
+#include <util/ISAtoSA.hpp>
+#include <util/assertions.hpp>
 #include <util/container.hpp>
 #include <util/sort/ternary_quicksort.hpp>
 #include <util/span.hpp>
 #include <util/string.hpp>
 
 namespace sacabench::qsufsort {
+
+constexpr size_t negative_mask = size_t(1) << (sizeof(size_t) * 8 - 1);
+constexpr size_t remove_tag_mask = std::numeric_limits<size_t>::max() >> 1;
 
 // Compare function for inital sorting
 struct compare_first_character {
@@ -42,7 +47,6 @@ public:
         } else if (b_out_of_bound) {
             return false;
         }
-
         return (V[a + h] < V[b + h]);
     }
     const util::container<size_t> V;
@@ -50,6 +54,193 @@ public:
 };
 
 class qsufsort {
+public:
+    // for trouble shooting
+    template <typename T>
+    static void print_array(T& arr) {
+        std::cout << "SA: ";
+        for (size_t i = 0; i < arr.size(); i++) {
+            std::cout << (bool(arr[i] & negative_mask) ? "-" : "")
+                      << ssize_t(arr[i] & remove_tag_mask) << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+    template <typename T, typename S>
+    static void print_isa(T& arr, S& out) {
+        std::cout << "V: ";
+        for (size_t i = 0; i < arr.size(); i++) {
+            std::cout << (bool(out[i] & negative_mask) ? i : arr[out[i]])
+                      << ", ";
+        }
+        std::cout << std::endl;
+    }
+    template <typename sa_index>
+    static void construct_sa(util::string_span text, size_t& alphabet_size,
+                             util::span<sa_index> out_sa) {
+
+        size_t n = text.size();
+
+        // catch trivial cases
+        if (n < 2)
+            return;
+
+        // init additional arrays
+        auto isa = util::make_container<sa_index>(n);
+        // init out_sa (necessary?)
+        for (size_t i = 0; i < n; ++i) {
+            out_sa[i] = i;
+        }
+        // init h (checked prefix length)
+        size_t h = 0;
+        // for are more readible while condition
+        bool is_sorted = false;
+        // comparing function for inital sort according to first character
+        auto compare_first_char_function = compare_first_character(text);
+        // Sort according to first character
+        util::sort::ternary_quicksort::ternary_quicksort(
+            out_sa, compare_first_char_function);
+        // Inital calculation of V and L
+        init_isa(text, out_sa, isa, h);
+        // TODO somehow replace, so only one function is needed..
+        // update_group_ranks(out_sa,V,compare_first_char_function,size_t(0),n-1);
+
+        // since we sorted accoring to first letter, increment h
+        ++h;
+        
+        while (!is_sorted) {
+            // comparing function, which compares the (i+h)-th ranks
+            auto compare_function = compare_ranks(isa, h);
+
+            size_t counter = 0;
+            // jump through array with group sizes
+            while (counter < out_sa.size()) {
+                // Sorted Group, check if negative bit is set
+                if (bool(out_sa[counter] & negative_mask)) {
+                    // Skip sorted group
+                    counter += out_sa[counter] & remove_tag_mask;
+                }
+                // unsorted group
+                else {
+                    size_t tmp = isa[out_sa[counter]];
+                    // sort unsorted group
+                    util::sort::ternary_quicksort::ternary_quicksort(
+                        out_sa.slice(counter, isa[out_sa[counter]] + 1),
+                        compare_function);
+                    // update ranks within group
+                    update_group_ranks(out_sa, isa, compare_function, counter,
+                                       tmp);
+                    // jump over updates group
+                    counter = tmp + 1;
+                }
+            }
+            // finally update group sizes
+            update_group_length(out_sa, isa);
+            // prefix doubling
+            h = h * 2;
+            is_sorted = ((out_sa[0] & remove_tag_mask) == n);
+        }
+        // transform isa to sa
+        util::isa2sa_simple_scan(util::span(isa), out_sa);
+    } // construct_sa
+
+private:
+    template <typename sa_index>
+    static void init_isa(util::string_span text, util::span<sa_index> out_sa,
+                         util::container<size_t>& isa, size_t h) {
+        size_t n = out_sa.size();
+        // rank of last element in out_sa is always n-1
+        isa[(out_sa[n - 1])] = n - 1;
+        for (size_t i = n - 2; i < n; --i) {
+            // Calculate V
+            // if same letter-> same group
+            if (text[out_sa[i + 1 + h]] == text[out_sa[i + h]]) {
+
+                isa[out_sa[i]] = isa[out_sa[i + 1]];
+            } else {
+                isa[out_sa[i]] = i;
+            }
+        }
+
+        update_group_length(out_sa, isa);
+    }
+    template <typename sa_index, typename key_func>
+    static void update_group_ranks(util::span<sa_index>& out_sa,
+                                   util::container<size_t>& isa, key_func& cmp,
+                                   sa_index start, sa_index end) {
+
+        const auto less = cmp;
+        const auto equal = util::as_equal(cmp);
+        const auto greater = util::as_greater(cmp);
+        // save highest group number of giver group
+
+        auto group_number = isa[out_sa[end]];
+        // for counting elements, which are still equal
+        size_t to_decrease = 1;
+        for (size_t index = end - 1; index >= start && index < end; --index) {
+            // in a sorted group should be a negative rank
+            DCHECK(!(bool(out_sa[index] & negative_mask)));
+
+            // if equal, increase counter
+            if (equal(out_sa[index], out_sa[index + 1])) {
+                ++to_decrease;
+            } else if (less(out_sa[index], out_sa[index + 1])) {
+                // if actually less, decrease by number of seen equal elements
+                // right behind
+                group_number -= to_decrease;
+                to_decrease = 1;
+            }
+            // no correct sorting
+            DCHECK(!greater(out_sa[index], out_sa[index + 1]))
+            // set group number
+            isa[out_sa[index]] = group_number;
+        }
+    }
+
+    template <typename sa_index>
+    static void update_group_length(util::span<sa_index>& out_sa,
+                                    util::container<size_t>& isa) {
+        size_t n = out_sa.size();
+        size_t unsorted_counter = 0;
+        size_t sorted_counter = 0;
+        bool sorted_group_started = false;
+        size_t dif = 0;
+
+        for (size_t i = n - 2; i < n; --i) {
+
+            // check if number in out_sa is negative
+            dif = (bool(out_sa[i + 1] & negative_mask) ? i + 1
+                                                       : isa[out_sa[i + 1]]) -
+                  (bool(out_sa[i] & negative_mask) ? i : isa[out_sa[i]]);
+
+            // count for last position..
+            unsorted_counter = (dif == 0) ? unsorted_counter + 1 : 0;
+
+            if (dif == 1) {
+                ++sorted_counter;
+                sorted_group_started = true;
+
+            } else {
+                if (sorted_group_started) {
+                    out_sa[i + 2] = negative_mask | sorted_counter;
+                    sorted_counter = 0;
+                    sorted_group_started = false;
+                }
+            }
+        }
+        dif = (bool(out_sa[1] & negative_mask) ? 1 : isa[out_sa[1]]) -
+              (bool(out_sa[0] & negative_mask) ? 0 : isa[out_sa[0]]);
+        // easier if use sentinal...
+        if (dif > 0) {
+            out_sa[0] = negative_mask | (++sorted_counter); //
+        }
+        // else, remain the same index
+    }
+
+}; // class qsufsort
+
+// keep naive version for comparison
+class qsufsort_naive {
 public:
     template <typename sa_index>
     static void construct_sa(util::string_span text, size_t& alphabet_size,
@@ -200,7 +391,6 @@ private:
         }
     }
 
-    // To be removed with improvements
     template <typename sa_index>
     static void update_L(util::span<sa_index>& out_sa,
                          util::container<size_t>& V,
