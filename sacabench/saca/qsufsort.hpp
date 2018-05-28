a991ae34da38c1d9f9a25aa394823d72851307fd
@foot <hermann.foot@tu-dortmund.de>
2018-05-28 19:44:40 +0200

    COmbined sorting and updating of unsorted groups

--- sacabench/saca/qsufsort.hpp
+++ sacabench/saca/qsufsort.hpp
@@ -13,10 +13,12 @@
 #include <util/span.hpp>
 #include <util/string.hpp>
 
+// TODO More checks
+
 namespace sacabench::qsufsort {
 
 constexpr size_t negative_mask = size_t(1) << (sizeof(size_t) * 8 - 1);
-constexpr size_t remove_tag_mask = std::numeric_limits<size_t>::max() >> 1;
+constexpr size_t remove_negative_mask = std::numeric_limits<size_t>::max() >> 1;
 
 // Compare function for inital sorting
 struct compare_first_character {
@@ -25,7 +27,7 @@ public:
         : input_text(_input_text) {}
     template <typename sa_index>
     bool operator()(const sa_index& a, const sa_index& b) const {
-        return ((input_text[a] - input_text[b]) < 0);
+        return ((input_text[a] < input_text[b]));
     }
     const util::string_span& input_text;
 };
@@ -47,6 +49,34 @@ public:
         } else if (b_out_of_bound) {
             return false;
         }
+
+        return (V[a + h] < V[b + h]);
+    }
+    const util::container<size_t>& V;
+    const size_t h;
+};
+
+// for naive case, V is not a reference, so changes doesnt effect the comapring
+// function inside one iteration
+struct compare_ranks_naive {
+public:
+    template <typename sa_index>
+    compare_ranks_naive(util::container<sa_index>& _V, size_t& _h)
+        : V(_V), h(_h) {}
+
+    template <typename sa_index>
+    // Warum const?
+    bool operator()(const sa_index& a, const sa_index& b) const {
+        bool a_out_of_bound = a + h >= V.size();
+        bool b_out_of_bound = b + h >= V.size();
+        if (a_out_of_bound && b_out_of_bound) {
+            return false;
+        } else if (a_out_of_bound) {
+            return true;
+        } else if (b_out_of_bound) {
+            return false;
+        }
+
         return (V[a + h] < V[b + h]);
     }
     const util::container<size_t> V;
@@ -61,7 +91,7 @@ public:
         std::cout << "SA: ";
         for (size_t i = 0; i < arr.size(); i++) {
             std::cout << (bool(arr[i] & negative_mask) ? "-" : "")
-                      << ssize_t(arr[i] & remove_tag_mask) << ", ";
+                      << ssize_t(arr[i] & remove_negative_mask) << ", ";
         }
         std::cout << std::endl;
     }
@@ -102,12 +132,9 @@ public:
             out_sa, compare_first_char_function);
         // Inital calculation of V and L
         init_isa(text, out_sa, isa, h);
-        // TODO somehow replace, so only one function is needed..
-        // update_group_ranks(out_sa,V,compare_first_char_function,size_t(0),n-1);
-
         // since we sorted accoring to first letter, increment h
         ++h;
-        
+
         while (!is_sorted) {
             // comparing function, which compares the (i+h)-th ranks
             auto compare_function = compare_ranks(isa, h);
@@ -118,27 +145,26 @@ public:
                 // Sorted Group, check if negative bit is set
                 if (bool(out_sa[counter] & negative_mask)) {
                     // Skip sorted group
-                    counter += out_sa[counter] & remove_tag_mask;
+                    counter += out_sa[counter] & remove_negative_mask;
                 }
                 // unsorted group
                 else {
+                    // size of group changes while updating, save for jumping to
+                    // right place
                     size_t tmp = isa[out_sa[counter]];
-                    // sort unsorted group
-                    util::sort::ternary_quicksort::ternary_quicksort(
-                        out_sa.slice(counter, isa[out_sa[counter]] + 1),
-                        compare_function);
+                    // sort and update unsorted group
+                    sort_and_update_group(out_sa, isa, compare_function,
+                                          counter, isa[out_sa[counter]]);
                     // update ranks within group
-                    update_group_ranks(out_sa, isa, compare_function, counter,
-                                       tmp);
                     // jump over updates group
                     counter = tmp + 1;
                 }
             }
-            // finally update group sizes
+            // update group sizes
             update_group_length(out_sa, isa);
             // prefix doubling
             h = h * 2;
-            is_sorted = ((out_sa[0] & remove_tag_mask) == n);
+            is_sorted = ((out_sa[0] & remove_negative_mask) == n);
         }
         // transform isa to sa
         util::isa2sa_simple_scan(util::span(isa), out_sa);
@@ -161,40 +187,8 @@ private:
                 isa[out_sa[i]] = i;
             }
         }
-
+        // Maybe directly in loop...
         update_group_length(out_sa, isa);
-    }
-    template <typename sa_index, typename key_func>
-    static void update_group_ranks(util::span<sa_index>& out_sa,
-                                   util::container<size_t>& isa, key_func& cmp,
-                                   sa_index start, sa_index end) {
-
-        const auto less = cmp;
-        const auto equal = util::as_equal(cmp);
-        const auto greater = util::as_greater(cmp);
-        // save highest group number of giver group
-
-        auto group_number = isa[out_sa[end]];
-        // for counting elements, which are still equal
-        size_t to_decrease = 1;
-        for (size_t index = end - 1; index >= start && index < end; --index) {
-            // in a sorted group should be a negative rank
-            DCHECK(!(bool(out_sa[index] & negative_mask)));
-
-            // if equal, increase counter
-            if (equal(out_sa[index], out_sa[index + 1])) {
-                ++to_decrease;
-            } else if (less(out_sa[index], out_sa[index + 1])) {
-                // if actually less, decrease by number of seen equal elements
-                // right behind
-                group_number -= to_decrease;
-                to_decrease = 1;
-            }
-            // no correct sorting
-            DCHECK(!greater(out_sa[index], out_sa[index + 1]))
-            // set group number
-            isa[out_sa[index]] = group_number;
-        }
     }
 
     template <typename sa_index>
@@ -209,6 +203,7 @@ private:
         for (size_t i = n - 2; i < n; --i) {
 
             // check if number in out_sa is negative
+            // if negative the group number is simply the index
             dif = (bool(out_sa[i + 1] & negative_mask) ? i + 1
                                                        : isa[out_sa[i + 1]]) -
                   (bool(out_sa[i] & negative_mask) ? i : isa[out_sa[i]]);
@@ -216,6 +211,7 @@ private:
             // count for last position..
             unsorted_counter = (dif == 0) ? unsorted_counter + 1 : 0;
 
+            // if difference between neighbours is 1, they are sorted elements
             if (dif == 1) {
                 ++sorted_counter;
                 sorted_group_started = true;
@@ -228,13 +224,81 @@ private:
                 }
             }
         }
+        // easier if use sentinal...
         dif = (bool(out_sa[1] & negative_mask) ? 1 : isa[out_sa[1]]) -
               (bool(out_sa[0] & negative_mask) ? 0 : isa[out_sa[0]]);
-        // easier if use sentinal...
         if (dif > 0) {
-            out_sa[0] = negative_mask | (++sorted_counter); //
+            out_sa[0] = negative_mask | (++sorted_counter);
         }
         // else, remain the same index
+    }
+
+    template <typename sa_index, typename key_func>
+    static void sort_and_update_group(util::span<sa_index>& full_array,
+                                      util::container<sa_index>& isa,
+                                      key_func& cmp, sa_index start,
+                                      sa_index end) {
+        auto out_sa = full_array.slice(start, end + 1);
+        size_t n = out_sa.size();
+        const auto equal = util::as_equal(cmp);
+        // recursion termination
+        if (n <= 1) {
+            // if only one element is in partition, set its rank
+            if (n == 1)
+                isa[full_array[start]] = start;
+            return;
+        }
+
+        if (n == 2) {
+            if (cmp(out_sa[1], out_sa[0])) {
+                std::swap(out_sa[0], out_sa[1]);
+            }
+            if (equal(out_sa[0], out_sa[1])) {
+                update_equal_partition_ranks(full_array, isa, start, end);
+            }
+            // if elements are not equal, it means that they are sorted, so they
+            // get different ranks
+            else {
+                isa[full_array[start]] = start;
+                isa[full_array[end]] = end;
+            }
+            return;
+        }
+
+        // Choose pivot according to array size
+        const sa_index pivot =
+            (n > util::sort::ternary_quicksort::MEDIAN_OF_NINE_THRESHOLD)
+                ? util::sort::ternary_quicksort::median_of_nine(out_sa, cmp)
+                : util::sort::ternary_quicksort::median_of_three(out_sa, cmp);
+        auto result =
+            util::sort::ternary_quicksort::partition(out_sa, cmp, pivot);
+        // sorts less partition recursivly
+        sort_and_update_group(full_array, isa, cmp, start,
+                              result.first + start - 1);
+        // update group ranks of equal partition
+        update_equal_partition_ranks(full_array, isa, result.first + start,
+                                     result.second + start - 1);
+        // sorts greater partition recursivly
+        sort_and_update_group(full_array, isa, cmp, result.second + start,
+                              n - 1 + start);
+        return;
+    }
+
+    template <typename sa_index>
+    static void update_equal_partition_ranks(util::span<sa_index> out_sa,
+                                             util::container<sa_index>& isa,
+                                             sa_index start, sa_index end) {
+
+        // in an unsorted group, every elements rank is the highest index, this
+        // group occuoies in the SA
+        for (size_t index = start; index <= end; ++index) {
+
+            if (bool(out_sa[index] & negative_mask)) {
+                isa[index] = end;
+            } else {
+                isa[out_sa[index]] = end;
+            }
+        }
     }
 
 }; // class qsufsort
@@ -276,7 +340,7 @@ public:
         while (!is_sorted) {
 
             // comparing function, which compares the (i+h)-th ranks
-            auto compare_function = compare_ranks(V, h);
+            auto compare_function = compare_ranks_naive(V, h);
 
             size_t counter = 0;
             // jump through array with group sizes
@@ -434,6 +498,6 @@ private:
         }
     }
 
-}; // class qsufsort
+}; // class qsufsort-naive
 
 } // namespace sacabench::qsufsort
