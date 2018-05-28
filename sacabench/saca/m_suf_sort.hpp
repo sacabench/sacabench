6c5365ec2c47b4a4353bfcb808619a3c6010e11b
rlpink <rosa.pink@tu-dortmund.de>
2018-05-28 14:08:09 +0200

    form_initial_chains finished and tested

--- sacabench/saca/m_suf_sort.hpp
+++ sacabench/saca/m_suf_sort.hpp
@@ -10,28 +10,46 @@
 #include <util/container.hpp>
 #include <util/span.hpp>
 #include <util/string.hpp>
+#include <util/bits.hpp>
 
-#include <tuple>
+//#include<iostream>
 #include <vector>
+#include <stack>
+#include <utility>
 
 namespace sacabench::m_suf_sort {
 template <typename sa_index>
 struct special_bits {
 private:
-    constexpr sa_index THIS_IS_MAGIC = ((sa_index)1) >> 1;
-    span<sa_index> isa;
+    util::span<sa_index> isa;
+    static constexpr sa_index negative_bit = ((sa_index)1) << (util::bits_of<sa_index> - 1);
 
 public:
+    static constexpr sa_index THIS_IS_MAGIC = ((sa_index)(-1)) >> 1;
+
     void set_null(sa_index index) { isa[index] = THIS_IS_MAGIC; }
     bool is_null(sa_index index) { return (isa[index] == THIS_IS_MAGIC); }
-    bool is_rank(sa_index index) { return (isa[index] < 0); }
-    bool set_rank(sa_index index, sa_index rank) {
-        isa[index] = -rank;
-        DCHECK_LT(-rank, 0);
+    bool is_rank(sa_index index) {
+        return (isa[index] & negative_bit) > 0;
     }
-    span<sa_index> get_span(){return(isa);}
+    void set_rank(sa_index index, sa_index rank) {
+        isa[index] = rank | negative_bit;
+    }
+    bool is_link(sa_index index) {
+        return !is_rank(index);
+    }
+    void set_link(sa_index index, sa_index link) {
+        isa[index] = link;
+    }
+    sa_index get(sa_index index) {
+        return isa[index];
+    }
+    util::span<sa_index> get_span(){return(isa);}
+    sa_index size() {
+        return isa.size();
+    }
 
-    special_bits(span<sa_index> isa_to_be) : isa(isa_to_be) {}
+    special_bits(util::span<sa_index> isa_to_be) : isa(isa_to_be) {}
 };
 
 struct m_suf_sort {
@@ -40,26 +58,64 @@ public:
     static void construct_sa(util::string_span text, size_t alphabet_size,
                              util::span<sa_index> out_sa) {
         // TODO: Check if sa_index type fits for text.size() and extra bits
+
+        // make a special_bits struct out of out_sa for isa
         special_bits isa(out_sa);
-        form_initial_chains(text, alphabet_size, isa);
+
+        // Initialize rank
+        sa_index rank = alphabet_size + 1;
+
+        // initialize chain_stack
+        std::stack<std::pair<sa_index, size_t>> chain_stack;
+        form_initial_chains(text, alphabet_size, isa, chain_stack);
+/*
+        // begin main loop:
+        while(chain_stack.size() > 0){
+            std::pair<sa_index, size_t> current_chain = chain_stack.pop();
+            sa_index chain_index = current_chain.first;
+            // if u-Chain is singleton rank it!
+            if(isa.is_null(chain_index)) {
+                assign_rank(chain_index, rank, isa, "uChain");
+            }
+        }
+    */
     }
 };
 
 // private:
 template <typename sa_index>
+void assign_rank(sa_index index, sa_index& rank, special_bits<sa_index>& isa, std::string state) {
+    if(state == "uChain") {
+        isa.set_rank(index, rank);
+        rank--;
+    }
+}
+template <typename sa_index>
 void form_initial_chains(util::string_span text, size_t alphabet_size,
-                         special_bits<sa_index> isa) {
+                         special_bits<sa_index>& isa, std::stack<std::pair<sa_index, size_t>>& cstack) {
     // contains all last elements of uChains
-    container<sa_index> uChain_links_ = make_container<sa_index>(alphabet_size);
-    special_bits uChain_links(span(uChain_links_));
+    // alternatively: use a hashmap and dynamically add new u-Chains
+    util::container<sa_index> uChain_links_ = util::make_container<sa_index>(alphabet_size + 1);
+
+    special_bits<sa_index> uChain_links{util::span<sa_index>(uChain_links_)};
     for (sa_index i = 0; i < alphabet_size + 1; i++) {
         // Set initial values for uChains: end-character, null
         uChain_links.set_null(i);
     }
+    // Link elements of same u-Chains within one left-to-right scan
     for (sa_index i = 0; i < isa.size(); i++) {
-        //TODO: Check if type s suffix, ignore type l at this point.
-        isa[i] = uChain_links[text[i]];
-        uChain_links[text[i]] = i;
+        //TODO: Check if type s suffix, ignore type l at this point!
+        isa.set_link(i, uChain_links.get(text[i]));
+        uChain_links.set_link(text[i], i);
+    }
+    // Fill uChains into cstack if they appear in the isa
+    for(sa_index i = 1; i < uChain_links.size() + 1; i++) {
+        // "largest" uChains first (so they are popped last)
+        sa_index j = uChain_links.size() - i;
+        if(!uChain_links.is_null(j)) {
+            std::pair<size_t, size_t> u_chain (uChain_links.get(j), 1);
+            cstack.push(u_chain);
+        }
     }
 }
 } // namespace sacabench::m_suf_sort
--- test/m_suf_sort_tests.cpp
+++ test/m_suf_sort_tests.cpp
@@ -6,31 +6,34 @@
 
 #include <gtest/gtest.h>
 #include "test/saca.hpp"
-
+#include <util/container.hpp>
+#include <util/span.hpp>
 #include <saca/m_suf_sort.hpp>
 #include <util/alphabet.hpp>
+#include <stack>
+#include <utility>
 
+using namespace sacabench;
+using namespace sacabench::util;
 using namespace sacabench::m_suf_sort;
 
 TEST(m_suf_sort, test) {
-    test::saca_corner_cases<m_suf_sort<>>();
+    //test::saca_corner_cases<m_suf_sort<>>();
 }
 
 TEST(m_suf_sort, test_form_initial_chains) {
-    util::string_span test_text = "caabaccaabacaa"_s;
+    util::string test_text = util::make_string("caabaccaabacaa");
     const alphabet alpha = alphabet(test_text);
-    alphabet effective_alpha = apply_effective_alphabet(test_text, alpha);
-    size_t alpha_size = effective_alpha.size;
-    container<sa_index> dummy = make_container(alpha_size);
-    span<sa_index> isa_span = span(dummy);
-    special_bits isa(isa_span);
-    //container<sa_index> expected_result = 
+    apply_effective_alphabet(test_text, alpha);
+    size_t alpha_size = alpha.size;
+    util::container<size_t> sa = make_container<size_t>(test_text.size());
+    span<size_t> sa_span = span(sa);
+    special_bits<size_t> isa{sa_span};
+    std::stack<std::pair<size_t, size_t>> chain_stack;
+    auto m = special_bits<size_t>::THIS_IS_MAGIC;
+    util::container<size_t> expected_result {m,m,1,m,2,0,5,4,7,3,8,6,10,12};
 
-    form_initial_chains(test_text, alpha_size, isa);
-    isa_span = isa.get_span();
-    ASSERT_EQ(isa_span, )
-
-
-
-
+    form_initial_chains<size_t>(span(test_text), alpha_size, isa, chain_stack);
+    ASSERT_EQ(chain_stack.size(), 3);
+    ASSERT_EQ(isa.get_span(), span(expected_result));
 }
