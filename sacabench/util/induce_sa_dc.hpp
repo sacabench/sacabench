1a07be7f05fa195020355fdeb0b8a37d013ef460
Johannes Bahne <johannes.bahne@tu-dortmund.de>
2018-05-30 16:13:58 +0200

    clang formatted

--- sacabench/util/induce_sa_dc.hpp
+++ sacabench/util/induce_sa_dc.hpp
@@ -1,17 +1,17 @@
 /*******************************************************************************
  * Copyright (C) 2018 Nico Bertram <nico.bertram@tu-dortmund.de>
  * Copyright (C) 2018 Johannes Bahne <johannes.bahne@tu-dortmund.de>
- * 
+ *
  * All rights reserved. Published under the BSD-3 license in the LICENSE file.
  ******************************************************************************/
+#include <algorithm>
 #include <tuple>
 #include <util/assertions.hpp>
 #include <util/container.hpp>
-#include <algorithm>
 
 #pragma once
 namespace sacabench::util {
-    template<typename C, typename T, typename I, typename S>
+template<typename C, typename T, typename I, typename S>
     //template C for input characters
     //template T for input string
     //template I for ISA
@@ -27,31 +27,35 @@ namespace sacabench::util {
     * This method works correct because of the difference cover idea.
     */
     void induce_sa_dc(const T& t_0, const I& isa_12, S& sa_0) {
-        
-        DCHECK_MSG(sa_0.size() == t_0.size(), "sa_0 must have the same length as t_0");
-        DCHECK_MSG(isa_12.size() >= t_0.size(), "isa_12 must have at least the same length as t_0");
 
-        //Container to store all tuples with the same length as sa_0
-        //Tuples contains a char and a rank 
-        auto sa_0_to_be_sorted = make_container<std::tuple<C, size_t, size_t>>(sa_0.size());
-        
-        size_t  start_pos_mod_2 = isa_12.size()/2 + ((isa_12.size()%2)!=0);
-        
-        for (size_t i = 0; i < sa_0.size(); i++) {
-            if(start_pos_mod_2 > i){
-                sa_0_to_be_sorted[i] = (std::tuple<C, size_t, size_t>(t_0[i], isa_12[i], 3 * i));
-            }
-            else{
-                sa_0_to_be_sorted[i] = (std::tuple<C, size_t, size_t>(t_0[i], 0, 3 * i));
-            }
-        }
-    
-        //TODO: sort Tupels with radix_sort
-        //radix_sort(sa0_to_be_sorted, sa0);
-        
-        std::sort(sa_0_to_be_sorted.begin(),sa_0_to_be_sorted.end());
-        for(size_t i = 0; i<sa_0_to_be_sorted.size();i++){
-            sa_0[i] = std::get<2>(sa_0_to_be_sorted[i]);
+    DCHECK_MSG(sa_0.size() == t_0.size(),
+               "sa_0 must have the same length as t_0");
+    DCHECK_MSG(isa_12.size() >= t_0.size(),
+               "isa_12 must have at least the same length as t_0");
+
+    // Container to store all tuples with the same length as sa_0
+    // Tuples contains a char and a rank
+    auto sa_0_to_be_sorted =
+        make_container<std::tuple<C, size_t, size_t>>(sa_0.size());
+
+    size_t start_pos_mod_2 = isa_12.size() / 2 + ((isa_12.size() % 2) != 0);
+
+    for (size_t i = 0; i < sa_0.size(); i++) {
+        if (start_pos_mod_2 > i) {
+            sa_0_to_be_sorted[i] =
+                (std::tuple<C, size_t, size_t>(t_0[i], isa_12[i], 3 * i));
+        } else {
+            sa_0_to_be_sorted[i] =
+                (std::tuple<C, size_t, size_t>(t_0[i], 0, 3 * i));
         }
     }
-}  // namespace sacabench::util
+
+    // TODO: sort Tupels with radix_sort
+    // radix_sort(sa0_to_be_sorted, sa0);
+
+    std::sort(sa_0_to_be_sorted.begin(), sa_0_to_be_sorted.end());
+    for (size_t i = 0; i < sa_0_to_be_sorted.size(); i++) {
+        sa_0[i] = std::get<2>(sa_0_to_be_sorted[i]);
+    }
+}
+} // namespace sacabench::util
