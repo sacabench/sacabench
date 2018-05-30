2a5e5c280362623cddf179a126b36dac5d411704
rlpink <rosa.pink@tu-dortmund.de>
2018-05-30 16:03:55 +0200

    fixed u-chain refinement. is now tested and functional!

--- sacabench/saca/m_suf_sort.hpp
+++ sacabench/saca/m_suf_sort.hpp
@@ -19,6 +19,9 @@
 #include <utility>
 
 namespace sacabench::m_suf_sort {
+
+template <typename sa_index> static constexpr sa_index END = ((sa_index)(-1)) >> 1;
+
 template <typename sa_index>
 struct special_bits {
 private:
@@ -26,10 +29,9 @@ private:
     static constexpr sa_index negative_bit = ((sa_index)1) << (util::bits_of<sa_index> - 1);
 
 public:
-    static constexpr sa_index THIS_IS_MAGIC = ((sa_index)(-1)) >> 1;
 
-    void set_null(sa_index index) { isa[index] = THIS_IS_MAGIC; }
-    bool is_null(sa_index index) { return (isa[index] == THIS_IS_MAGIC); }
+    void set_END(sa_index index) { isa[index] = END<sa_index>; }
+    bool is_END(sa_index index) { return (isa[index] == END<sa_index>); }
     bool is_rank(sa_index index) {
         return (isa[index] & negative_bit) > 0;
     }
@@ -55,6 +57,7 @@ public:
 
 struct m_suf_sort {
 public:
+
     template <typename sa_index>
     static void construct_sa(util::string_span text, size_t alphabet_size,
                              util::span<sa_index> out_sa) {
@@ -76,7 +79,7 @@ public:
             std::pair<sa_index, size_t> current_chain = chain_stack.pop();
             sa_index chain_index = current_chain.first;
             // if u-Chain is singleton rank it!
-            if(isa.is_null(chain_index)) {
+            if(isa.is_END(chain_index)) {
                 assign_rank(chain_index, rank, isa, "uChain");
             }
         }
@@ -154,54 +157,34 @@ void refine_uChain(util::string_span text, special_bits<sa_index>& isa,
      std::stack<std::pair<sa_index, sa_index>>& cstack, util::span<sa_index> new_chain_IDs, sa_index length){
          compare_uChain_elements comparator(length, text);
          util::sort::introsort(new_chain_IDs, comparator);
-         // TODO: Test if new_chain_IDs is sorted properly after this step!
 
-         //TODO: Planning what exactly should be done here?!?!
+         // last index that is to be linked
+         sa_index last_ID = END<sa_index>;
 
-         // set initial value to null as it marks the beginning of a u-Chain
-         isa.set_null(new_chain_IDs[0]);
-         for(sa_index i = 0; i < new_chain_IDs.size() - 1; i++) {
-             auto current_element = text[new_chain_IDs[i] + length];
-             auto next_element = text[new_chain_IDs[i+1] + length];
+         for(sa_index i = 1; i < new_chain_IDs.size(); i++) {
+             auto current_element = text[new_chain_IDs[i-1] + length];
+             auto next_element = text[new_chain_IDs[i] + length];
+             sa_index current_ID = new_chain_IDs[i-1];
+
+             // no matter what, we first link the last element:
+             isa.set_link(current_ID, last_ID);
+
              // does this chain continue?
              if(current_element != next_element) {
-                 // this element marks the end of a chain, push the new chain on stack!
-                 std::pair<sa_index, sa_index> new_chain (new_chain_IDs[i], length + 1);
+
+                 // this element marks the end of a chain, push the new chain on stack
+                 std::pair<sa_index, sa_index> new_chain (current_ID, length + 1);
                  cstack.push(new_chain);
-                 // same time, this means next element is beginning of a new chain:
-                 // set it to null in isa
-                 isa.set_null(new_chain_IDs[i+1]);
+
+                 // next link should be set to END to mark the beginning of a new chain
+                 last_ID = END<sa_index>;
+             }
+             else {
+                 last_ID = current_ID;
              }
          }
-}
-
-//TODO: Rubbish! This is NOT mSufSort, this is some buckety-thingy going on here!
-template <typename sa_index>
-void form_initial_chains(util::string_span text, size_t alphabet_size,
-                         special_bits<sa_index>& isa, std::stack<std::pair<sa_index, sa_index>>& cstack) {
-    // contains all last elements of uChains
-    // alternatively: use a hashmap and dynamically add new u-Chains
-    util::container<sa_index> uChain_links_ = util::make_container<sa_index>(alphabet_size + 1);
-
-    special_bits<sa_index> uChain_links{util::span<sa_index>(uChain_links_)};
-    for (sa_index i = 0; i < alphabet_size + 1; i++) {
-        // Set initial values for uChains: end-character, null
-        uChain_links.set_null(i);
-    }
-    // Link elements of same u-Chains within one left-to-right scan
-    for (sa_index i = 0; i < isa.size(); i++) {
-        //TODO: Check if type s suffix, ignore type l at this point!
-        isa.set_link(i, uChain_links.get(text[i]));
-        uChain_links.set_link(text[i], i);
-    }
-    // Fill uChains into cstack if they appear in the isa
-    for(sa_index i = 1; i < uChain_links.size() + 1; i++) {
-        // "largest" uChains first (so they are popped last)
-        sa_index j = uChain_links.size() - i;
-        if(!uChain_links.is_null(j)) {
-            std::pair<size_t, size_t> u_chain (uChain_links.get(j), 1);
-            cstack.push(u_chain);
-        }
-    }
+         //last element is automatically beginning of the (lexikographically) smallest chain
+         std::pair<sa_index, sa_index> new_chain (new_chain_IDs[new_chain_IDs.size() - 1], length + 1);
+         cstack.push(new_chain);
 }
 } // namespace sacabench::m_suf_sort
