8d2bb10089ad72504e3be5e2017c58251b242722
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2018-06-01 14:48:41 +0200

    initial design for ringbuffer

---
+++ sacabench/util/ringbuffer.hpp
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (C) 2018 Marvin Böcker <marvin.boecker@tu-dortmund.de>
+ *
+ * All rights reserved. Published under the BSD-3 license in the LICENSE file.
+ ******************************************************************************/
+
+#pragma once
+
+#include <util/container.hpp>
+#include <util/span.hpp>
+
+namespace sacabench::util {
+template <typename content>
+class ringbuffer {
+public:
+    inline ringbuffer(const size_t capacity)
+        : memory(make_container<content>(capacity)), start(0),
+          end(capacity - 1) {}
+
+    inline void push_front(const content e) {
+        DCHECK_EQ(is_full(), false);
+        memory[end] = e;
+        end--;
+    }
+
+    inline void push_back(const content e) {
+        DCHECK_EQ(is_full(), false);
+        memory[start] = e;
+        start++;
+    }
+
+    inline void traverse(const util::span<content> out) const {
+        const auto wrapping_add_one = [&](size_t a) {
+            return (a + 1) % size();
+        };
+
+        for (size_t idx = wrapping_add_one(end), i = 0; i < size();
+             idx = wrapping_add_one(idx), ++i) {
+            out[i] = memory[idx];
+        }
+    }
+
+    inline size_t size() const { return memory.size(); }
+
+    inline bool is_full() const { return start == end + 1; }
+
+private:
+    util::container<content> memory;
+    size_t start;
+    size_t end;
+};
+} // namespace sacabench::util
---
+++ test/ringbuffer.cpp
@@ -0,0 +1,100 @@
+/*******************************************************************************
+ * Copyright (C) 2018 Marvin Böcker <marvin.boecker@tu-dortmund.de>
+ *
+ * All rights reserved. Published under the BSD-3 license in the LICENSE file.
+ ******************************************************************************/
+
+#include <gtest/gtest.h>
+#include <random>
+#include <util/container.hpp>
+#include <util/ringbuffer.hpp>
+
+using namespace sacabench::util;
+
+TEST(ringbuffer, create) {
+    ringbuffer<size_t> rb(16);
+    ASSERT_EQ(rb.size(), 16);
+}
+
+TEST(ringbuffer, push) {
+    ringbuffer<size_t> rb(16);
+
+    for (size_t i = 5; i < 16; ++i) {
+        rb.push_back(i);
+    }
+
+    for (size_t i = 0; i < 5; ++i) {
+        size_t j = 4 - i;
+        rb.push_front(j);
+    }
+
+    auto c = make_container<size_t>(16);
+    rb.traverse(c);
+
+    for (size_t i = 0; i < 16; ++i) {
+        ASSERT_EQ(c[i], i);
+    }
+}
+
+TEST(ringbuffer, push2) {
+    ringbuffer<size_t> rb(16);
+
+    for (size_t i = 5; i < 10; ++i) {
+        rb.push_back(i);
+    }
+
+    for (size_t i = 0; i < 5; ++i) {
+        size_t j = 4 - i;
+        rb.push_front(j);
+    }
+
+    auto c = make_container<size_t>(16);
+    rb.traverse(c);
+
+    for (size_t i = 0; i < 10; ++i) {
+        ASSERT_EQ(c[i], i);
+    }
+}
+
+TEST(ringbuffer, full) {
+    ringbuffer<size_t> rb(16);
+
+    for (size_t i = 0; i < 16; ++i) {
+        rb.push_back(i);
+    }
+
+    try {
+        rb.push_back(40);
+    } catch (...) {
+        ASSERT_FALSE(false);
+        return;
+    }
+
+    try {
+        rb.push_front(40);
+    } catch (...) {
+        ASSERT_FALSE(false);
+        return;
+    }
+
+    ASSERT_FALSE(true);
+}
+
+TEST(ringbuffer, random) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(false, true);
+
+    for (size_t i = 0; i < 100000; ++i) {
+        ringbuffer<size_t> rb(5);
+        for (size_t j = 0; j < 5; ++j) {
+            bool use_front = dist(gen);
+            if (use_front) {
+                rb.push_front(i);
+            } else {
+                rb.push_back(i);
+            }
+        }
+        ASSERT_EQ(rb.size(), 5);
+    }
+}
