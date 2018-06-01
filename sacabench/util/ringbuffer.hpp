707c51798c7dfe695c37ad1b67cc6f420936b325
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2018-06-01 18:17:07 +0200

    Revert "Fix is_full() when using deletion"
    This reverts commit 1c4b4fa3320bd30c821eae5bab759d30ef9b0c07.

--- sacabench/util/ringbuffer.hpp
+++ sacabench/util/ringbuffer.hpp
@@ -42,7 +42,7 @@ public:
 
     inline size_t size() const { return memory.size(); }
 
-    inline bool is_full() const { return start == (end + 1) % size(); }
+    inline bool is_full() const { return start == end + 1; }
 
 private:
     util::container<content> memory;
