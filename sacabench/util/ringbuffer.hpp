e760a301ffc793abc56c2e10feab1767c46eca0e
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2018-06-03 16:36:13 +0200

    Re-write of ringbuffer

--- sacabench/util/ringbuffer.hpp
+++ sacabench/util/ringbuffer.hpp
@@ -13,40 +13,59 @@ namespace sacabench::util {
 template <typename content>
 class ringbuffer {
 public:
-    inline ringbuffer(const size_t capacity)
-        : memory(make_container<content>(capacity)), start(0),
-          end(capacity - 1) {}
-
-    inline void push_front(const content e) {
-        DCHECK_EQ(is_full(), false);
-        memory[end] = e;
-        end--;
-    }
+    inline ringbuffer(util::span<content> _memory) : memory(_memory), front(), back() {}
 
     inline void push_back(const content e) {
-        DCHECK_EQ(is_full(), false);
-        memory[start] = e;
-        start++;
+        DCHECK(!is_full());
+        back = memory.slice(0, back.size() + 1);
+        back[back.size() - 1] = std::move(e);
     }
 
-    inline void traverse(const util::span<content> out) const {
-        const auto wrapping_add_one = [&](size_t a) {
-            return (a + 1) % size();
-        };
+    inline void push_front(const content e) {
+        DCHECK(!is_full());
+        front = memory.slice(capacity() - (front.size() + 1));
+        front[0] = std::move(e);
+    }
 
-        for (size_t idx = wrapping_add_one(end), i = 0; i < size();
-             idx = wrapping_add_one(idx), ++i) {
-            out[i] = memory[idx];
+    inline bool is_full() const {
+        return size() >= capacity();
+    }
+
+    inline size_t capacity() const {
+        return memory.size();
+    }
+
+    inline size_t size() const {
+        return front.size() + back.size();
+    }
+
+    inline void print() const {
+        std::cout << memory << std::endl;
+        std::cout << front << back << std::endl;
+    }
+
+    template<typename Fn>
+    inline void for_each(const Fn fn) const {
+        for(const content& e : front) {
+            fn(e);
+        }
+        for(const content& e : back) {
+            fn(e);
         }
     }
 
-    inline size_t size() const { return memory.size(); }
-
-    inline bool is_full() const { return start == end + 1; }
+    inline void copy_into(util::span<content> s) const {
+        size_t i = 0;
+        DCHECK_LE(size(), s.size());
+        for_each([&](const content& e){
+            s[i] = e;
+            ++i;
+        });
+    }
 
 private:
-    util::container<content> memory;
-    size_t start;
-    size_t end;
+    util::span<content> memory;
+    util::span<content> front;
+    util::span<content> back;
 };
 } // namespace sacabench::util
--- test/ringbuffer.cpp
+++ test/ringbuffer.cpp
@@ -9,82 +9,81 @@
 #include <util/container.hpp>
 #include <util/ringbuffer.hpp>
 #include <util/macros.hpp>
+#include <util/is_sorted.hpp>
 
 using namespace sacabench::util;
 
 TEST(ringbuffer, create) {
-    ringbuffer<size_t> rb(16);
-    ASSERT_EQ(rb.size(), 16u);
+    auto memory = make_container<size_t>(16);
+    ringbuffer<size_t> rb(memory);
+    ASSERT_EQ(rb.size(), 0u);
+    ASSERT_EQ(rb.capacity(), 16u);
 }
 
 TEST(ringbuffer, push) {
-    ringbuffer<size_t> rb(16);
+    auto memory = make_container<size_t>(16);
+    ringbuffer<size_t> rb(memory);
 
-    for (size_t i = 5; i < 16; ++i) {
-        rb.push_back(i);
-    }
+    ASSERT_EQ(rb.size(), 0u);
+    ASSERT_EQ(rb.capacity(), 16u);
 
-    for (size_t i = 0; i < 5; ++i) {
-        size_t j = 4 - i;
-        rb.push_front(j);
-    }
+    rb.push_back(5);
+    rb.push_back(6);
 
-    auto c = make_container<size_t>(16);
-    rb.traverse(c);
+    rb.push_front(4);
+    rb.push_front(3);
+    rb.push_front(2);
+    rb.push_front(1);
 
-    for (size_t i = 0; i < 16; ++i) {
-        ASSERT_EQ(c[i], i);
-    }
+    rb.push_back(7);
 }
 
-TEST(ringbuffer, push2) {
-    ringbuffer<size_t> rb(16);
+TEST(ringbuffer, push_and_foreach) {
+    auto memory = make_container<size_t>(16);
+    ringbuffer<size_t> rb(memory);
 
-    for (size_t i = 5; i < 10; ++i) {
-        rb.push_back(i);
-    }
+    ASSERT_EQ(rb.size(), 0u);
+    ASSERT_EQ(rb.capacity(), 16u);
 
-    for (size_t i = 0; i < 5; ++i) {
-        size_t j = 4 - i;
-        rb.push_front(j);
-    }
+    rb.push_back(5);
+    rb.push_back(6);
 
-    auto c = make_container<size_t>(16);
-    rb.traverse(c);
+    rb.push_front(4);
+    rb.push_front(3);
+    rb.push_front(2);
+    rb.push_front(1);
 
-    for (size_t i = 0; i < 10; ++i) {
-        ASSERT_EQ(c[i], i);
-    }
-}
+    rb.push_back(7);
 
-TEST(ringbuffer, full) {
-    ringbuffer<size_t> rb(16);
-
-    for (size_t i = 0; i < 16; ++i) {
-        rb.push_back(i);
-    }
-
-    bool back_exception = false, front_exception = false;
-
-    try {
-        rb.push_back(40);
-    } catch (...) {
-        back_exception = true;
-    }
-
-    try {
-        rb.push_front(40);
-    } catch (...) {
-        front_exception = true;
-    }
-
-    (void) back_exception;
-    (void) front_exception;
-
-    IF_DEBUG({
-        ASSERT_TRUE(back_exception);
-        ASSERT_TRUE(front_exception);
+    auto sorted_memory = std::vector<size_t>();
+    rb.for_each([&](auto e) {
+        sorted_memory.push_back(e);
     });
+
+    ASSERT_TRUE(is_sorted(span(sorted_memory)));
+}
+
+TEST(ringbuffer, push_and_copy_into) {
+    auto memory = make_container<size_t>(16);
+    ringbuffer<size_t> rb(memory);
+
+    ASSERT_EQ(rb.size(), 0u);
+    ASSERT_EQ(rb.capacity(), 16u);
+
+    rb.push_back(5);
+    rb.push_back(6);
+
+    rb.push_front(4);
+    rb.push_front(3);
+    rb.push_front(2);
+    rb.push_front(1);
+
+    rb.push_back(7);
+
+    auto sorted_memory = make_container<size_t>(7);
+    rb.copy_into(sorted_memory);
+
+    ASSERT_TRUE(is_sorted(span<size_t>(sorted_memory)));
 }
 
 TEST(ringbuffer, random) {
@@ -92,16 +91,19 @@ TEST(ringbuffer, random) {
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dist(false, true);
 
-    for (size_t i = 0; i < 100000; ++i) {
-        ringbuffer<size_t> rb(5);
+    auto memory = make_container<size_t>(5);
+
+    for (size_t i = 0; i < 1000000; ++i) {
+        ringbuffer<size_t> rb(memory);
         for (size_t j = 0; j < 5; ++j) {
             bool use_front = dist(gen);
             if (use_front) {
-                rb.push_front(i);
+                rb.push_front(j);
             } else {
-                rb.push_back(i);
+                rb.push_back(j);
             }
         }
+
         ASSERT_EQ(rb.size(), 5u);
     }
 }
