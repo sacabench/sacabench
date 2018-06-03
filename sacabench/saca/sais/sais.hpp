c9b65f2ec9b33c44ea37b841fea55d51d8220810
Christopher Poeplau <christopher.poeplau@tu-dortmund.de>
2018-06-03 16:12:26 +0200

    template based sais

--- sacabench/saca/sais/sais.hpp
+++ sacabench/saca/sais/sais.hpp
@@ -6,10 +6,6 @@
 
 #pragma once
 
-#include <iostream>
-#include <vector>
-
-#include <util/sort/std_sort.hpp>
 #include <util/string.hpp>
 #include <util/container.hpp>
 #include <util/span.hpp>
@@ -19,103 +15,114 @@ namespace sacabench::saca::sais {
 using namespace sacabench::util;
 
 class Sais {
-    public:
-        #define chr(i) (cs == sizeof(ssize) ? ((ssize*)s)[i] : ((unsigned char*)s)[i])
+    public:        
+        static const size_t L_Type = 0;
+        static const size_t S_Type = 1;
 
-        static const ssize L_Type = 0;
-        static const ssize S_Type = 1;
+        template<typename T>
+        static T get_char_value(int position, unsigned char* s) {
+            return ((T*)s)[position];
+        }
 
-        static void compute_types(ssize* t, ssize* p_1, unsigned char* s, ssize len, ssize cs) {
+        template<typename T>
+        static void compute_types(size_t* t, size_t* p_1, unsigned char* s, size_t len) {
             t[len - 1] = S_Type;
 
             for (ssize i = len - 2; i >= 0; i--) {
-                if (chr(i + 1) < chr(i)) {
+                if (get_char_value<T>(i + 1, s) < get_char_value<T>(i, s)) {
                     t[i] = L_Type;
                     if (t[i + 1] == S_Type) { p_1[i + 1] = 1; }
                 }
-                else if (chr(i + 1) > chr(i)) { t[i] = S_Type; }
+                else if (get_char_value<T>(i + 1, s) > get_char_value<T>(i, s)) { t[i] = S_Type; }
                 else { t[i] = t[i + 1]; }
             }
         }
 
-        static void generate_buckets(unsigned char* s, ssize len, ssize* buckets, ssize K, bool end, ssize cs) {
-            ssize sum = 0;
+        template<typename T>
+        static void generate_buckets(unsigned char* s, size_t len, size_t* buckets, size_t K, bool end) {
+            size_t sum = 0;
 
-            for (ssize i = 0; i <= K; i++) { buckets[i] = 0; }
+            for (size_t i = 0; i <= K; i++) { buckets[i] = 0; }
 
             // bucket size for each char
-            for (ssize i = 0; i < len; i++) { buckets[chr(i)]++; }
+            for (size_t i = 0; i < len; i++) { buckets[get_char_value<T>(i, s)]++; }
 
             // sum up to bucket ends
-            for (ssize i = 0; i <= K; i++) {
+            for (size_t i = 0; i <= K; i++) {
                 sum += buckets[i];
                 buckets[i] = (end ? sum : sum - buckets[i]);
             }
         }
 
-        static void induce_L_Types(unsigned char* s, ssize len, ssize *buckets, ssize K, bool end, ssize* SA, ssize* t, ssize cs) {
-            generate_buckets(s, len, buckets, K, end, cs);
-            for (ssize i = 0; i < len; i++) {
+        template<typename T>
+        static void induce_L_Types(unsigned char* s, size_t len, size_t *buckets, size_t K, bool end, ssize* SA, size_t* t) {
+            generate_buckets<T>(s, len, buckets, K, end);
+            for (size_t i = 0; i < len; i++) {
                 ssize pre_index = SA[i] - 1; // pre index of the ith suffix array position
 
                 if (pre_index >= 0 && t[pre_index] == L_Type) { // pre index is type L
-                    SA[buckets[chr(pre_index)]++] = pre_index; // "sort" index in the bucket
+                    SA[buckets[get_char_value<T>(pre_index, s)]++] = pre_index; // "sort" index in the bucket
                 }
             }
         }
 
-        static void induce_S_Types(unsigned char* s, ssize len, ssize *buckets, ssize K, bool end, ssize* SA, ssize *t, ssize cs) {
-            generate_buckets(s, len, buckets, K, end, cs);
+        template<typename T>
+        static void induce_S_Types(unsigned char* s, size_t len, size_t *buckets, size_t K, bool end, ssize* SA, size_t *t) {
+            generate_buckets<T>(s, len, buckets, K, end);
             for (ssize i = len - 1; i >= 0; i--) {
                 ssize pre_index = SA[i] - 1;
 
                 if (pre_index >= 0 && t[pre_index] == S_Type) {
-                    SA[--buckets[chr(pre_index)]] = pre_index;
+                    SA[--buckets[get_char_value<T>(pre_index, s)]] = pre_index;
                 }
             }
         }
 
-        static void SAIS(unsigned char* s, ssize* SA, ssize len, ssize K, ssize cs) {
-            ssize* t = new ssize[len]();
-            ssize* p_1 = new ssize[len]();
-            ssize *buckets = new ssize[K + 1]();
+        template<typename T>
+        static void SAIS(unsigned char* s, ssize* SA, size_t len, ssize K) {
+            size_t* t = new size_t[len]();
+            size_t* p_1 = new size_t[len]();
+            size_t *buckets = new size_t[K + 1]();
 
-            compute_types(t, p_1, s, len, cs);
+            compute_types<T>(t, p_1, s, len);
 
-            generate_buckets(s, len, buckets, K, true, cs);
+            generate_buckets<T>(s, len, buckets, K, true);
             // Initialize each entry in SA with -1
-            for (ssize i = 0; i < len; i++) { SA[i] = -1; }
-            // iterate from left to right (starting by 1 cause 0 can never be LMS) and put LMS to end of the bucket and move bucket's tail backwards
-            for (ssize i = 1; i < len; i++) {
+            for (size_t i = 0; i < len; i++) { SA[i] = -1; }
+            // iterate from left to right (starting by 1 cause 0 can never be LMS) and put 
+            // LMS to end of the bucket and move bucket's tail backwards
+            for (size_t i = 1; i < len; i++) {
                 if (p_1[i] == 1) {
-                    SA[--buckets[chr(i)]] = i;
+                    SA[--buckets[get_char_value<T>(i, s)]] = i;
                 }
             }
 
             // sort LMS substrings
-            induce_L_Types(s, len, buckets, K, false, SA, t, cs);
-            induce_S_Types(s, len, buckets, K, true, SA, t, cs);
+            induce_L_Types<T>(s, len, buckets, K, false, SA, t);
+            induce_S_Types<T>(s, len, buckets, K, true, SA, t);
 
             // because we have at most n/2 LMS, we can store the sorted indices in the first half of the SA
             ssize n1 = 0;
-            for (ssize i = 0; i < len; i++) {
+            for (size_t i = 0; i < len; i++) {
                 if (p_1[SA[i]] == 1) {
                     SA[n1++] = SA[i];
                 }
             }
 
             // All LMS are now stored in the (at most) first half of the SA, so the rest half of the suffix array can be used 
-            for (ssize i = n1; i < len; i++) { SA[i] = -1; }
+            for (size_t i = n1; i < len; i++) { SA[i] = -1; }
 
-            // The given names correspond to the buckets the LMS are sorted ssizeo. To find the names, the strings have to be compared by its type and lexicographical value per char
+            // The given names correspond to the buckets the LMS are sorted ssizeo. 
+            // To find the names, the strings have to be compared by its type and lexicographical value per char
             ssize name = 0;
             ssize previous_LMS = -1;
             for (ssize i = 0; i < n1; i++) { // max n/2 iterations
                 bool diff = false;
                 // compare types and chars
                 ssize current_LMS = SA[i];
-                for (ssize j = 0; j < len; j++) {
-                    if (previous_LMS == -1 || chr(current_LMS + j) != chr(previous_LMS + j) || t[current_LMS + j] != t[previous_LMS + j]) {
+                for (size_t j = 0; j < len; j++) {
+                    if (previous_LMS == -1 || get_char_value<T>(current_LMS + j, s) != get_char_value<T>(previous_LMS + j, s) || 
+                        t[current_LMS + j] != t[previous_LMS + j]) {
                         diff = true;
                         break;
                     }
@@ -145,7 +152,7 @@ class Sais {
             ssize* s1 = (SA + len - n1);
             if (name < n1) {
                 // new string that consists of ssizeegers now, as they represent the names
-                SAIS((unsigned char*)s1, SA1, n1, name - 1, sizeof(ssize));
+                SAIS<size_t>((unsigned char*)s1, SA1, n1, name - 1);
             }
             else {
                 for (ssize i = 0; i < n1; i++) {
@@ -154,9 +161,9 @@ class Sais {
             }
 
             // induce the final SA
-            generate_buckets(s, len, buckets, K, true, cs);
-            ssize j;
-            for (ssize i = 1, j = 0; i < len; i++) {
+            generate_buckets<T>(s, len, buckets, K, true);
+            size_t j;
+            for (size_t i = 1, j = 0; i < len; i++) {
                 if (p_1[i] == 1) {
                     s1[j++] = i;
                 }
@@ -164,17 +171,17 @@ class Sais {
             for (ssize i = 0; i < n1; i++) {
                 SA1[i] = s1[SA1[i]];
             }
-            for (ssize i = n1; i < len; i++) {
+            for (size_t i = n1; i < len; i++) {
                 SA[i] = -1;
             }
             for (ssize i = n1 - 1; i >= 0; i--) {
                 j = SA[i];
                 SA[i] = -1;
-                SA[--buckets[chr(j)]] = j;
+                SA[--buckets[get_char_value<T>(j, s)]] = j;
             }
 
-            induce_L_Types(s, len, buckets, K, false, SA, t, cs);
-            induce_S_Types(s, len, buckets, K, true, SA, t, cs);
+            induce_L_Types<T>(s, len, buckets, K, false, SA, t);
+            induce_S_Types<T>(s, len, buckets, K, true, SA, t);
 
             delete[] buckets;
             delete[] t;
@@ -183,10 +190,19 @@ class Sais {
  
 	    template<typename sa_index>
 	    static void construct_sa(util::string_span text, size_t alphabet_size, util::span<sa_index> out_sa) {
-            ssize* SA = new ssize[text.size()];	        
 	        
-            SAIS((unsigned char*)text.data(), SA, text.size(), alphabet_size, 1);
-            for (size_t i = 0; i < text.size(); i++) { out_sa[i] = SA[i];} }   
+	        // obsolete once the null-byte is appended by the framework
+            string copy = string(text.size() + 1);
+            for (size_t i = 0; i < text.size(); i++) { copy[i] = text[i]; }
+            copy[text.size()] = 0;
+            
+            size_t len = copy.size();
+            ssize* SA = new ssize[len]();	        
+	        
+            SAIS<unsigned char>((unsigned char*)copy.data(), SA, len, alphabet_size);
+            
+            for (size_t i = 1; i < len; i++) { out_sa[i - 1] = SA[i]; } 
+        }   
 	};
 } 
 
