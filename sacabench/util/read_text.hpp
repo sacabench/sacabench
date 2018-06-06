2cd258f6126ead9db5697f8ef777f967981934b8
David Piper <david.piper@udo.edu>
2018-06-06 16:46:04 +0200

    Removed constructing string in read text. It now uses a span which is given as a parameter. Changed tests to reflect this change.

--- sacabench/util/read_text.hpp
+++ sacabench/util/read_text.hpp
@@ -31,53 +31,13 @@ namespace sacabench::util {
          * \brief Reads content of a txt file into a string.
          * This function reads the content of a text file at the given path bytewise.
          */
-        string read_text() {
-
-            character* data = 0;
-            data = new character[size];
+        inline void read_text(string_span out_text) {
+            DCHECK_EQ(out_text.size(), this->size);
 
             file.seekg(0, std::ios::beg); // set the pointer to the beginning
-            file.read( (char*) data, size );
-
-            auto result = span(data, size);
-            return make_string(result);
+            file.read( (char*) out_text.data(), out_text.size());
         }
     };
-
-    /**
-     * \brief Reads content of a txt file into a string.
-     * This function reads the content of a text file at the given path line by line.
-     *
-     * \param filepath Path of the file to be read.
-     */
-    string read_text_old (std::string filepath) {
-
-        std::vector<character> text;
-        std::string line;
-        std::ifstream filestream(filepath.c_str());
-
-        // Make sure the file could be opened.
-        if (!filestream) {
-            std::cout << "Error! Incorrect file." << std::endl;
-            exit(EXIT_FAILURE);
-        }
-
-        // Insert first line.
-        std::getline(filestream, line);
-        std::copy(line.begin(), line.end(), std::back_inserter(text));
-
-        // Insert rest of lines together with a '\n'.
-        while (!filestream.eof()) {
-            text.push_back('\n');
-            std::getline(filestream, line);
-            // Insert chars of current line into string text.
-            std::copy(line.begin(), line.end(), std::back_inserter(text));
-        }
-
-        // TODO: Instead of making a copy here, replace implementation above
-        // with something that directly writes into a `util::string`.
-        return make_string(span(text));
-    }
 }
 
 /******************************************************************************/
--- test/read_text_tests.cpp
+++ test/read_text_tests.cpp
@@ -17,37 +17,11 @@ TEST(read_text, read_text_without_trailing_newline) {
     // we have to go to the prevoius directory (sacabench) and then into the test directory.
     std::string path = "../test/read_text_tests.txt";
     auto context = read_text_context(path);
-    string input = context.read_text();
+    auto container = make_container<character>(context.size);
+    context.read_text(container);
 
-    ASSERT_EQ(input.size(), context.size);
-    ASSERT_EQ(input.size(), size_t(27));
-    ASSERT_EQ(input[0], 'H');
-    ASSERT_EQ(input[1], 'e');
-    ASSERT_EQ(input[2], 'l');
-    ASSERT_EQ(input[3], 'l');
-    ASSERT_EQ(input[4], 'o');
-    ASSERT_EQ(input[5], '!');
-    ASSERT_EQ(input[6], '\n');
-    ASSERT_EQ(input[7], 'r');
-    ASSERT_EQ(input[8], 'e');
-    ASSERT_EQ(input[9], 'a');
-    ASSERT_EQ(input[10], 'd');
-    ASSERT_EQ(input[11], '_');
-    ASSERT_EQ(input[12], 't');
-    ASSERT_EQ(input[13], 'e');
-    ASSERT_EQ(input[14], 'x');
-    ASSERT_EQ(input[15], 't');
-    ASSERT_EQ(input[16], ' ');
-    ASSERT_EQ(input[17], 't');
-    ASSERT_EQ(input[18], 'e');
-    ASSERT_EQ(input[19], 's');
-    ASSERT_EQ(input[20], 't');
-    ASSERT_EQ(input[21], '.');
-    ASSERT_EQ(input[22], '\n');
-    ASSERT_EQ(input[23], 'B');
-    ASSERT_EQ(input[24], 'y');
-    ASSERT_EQ(input[25], 'e');
-    ASSERT_EQ(input[26], '!');
+    ASSERT_EQ(container.size(), size_t(27));
+    ASSERT_EQ(container, "Hello!\nread_text test.\nBye!"_s);
 }
 
 TEST(read_text, read_text_with_trailing_newline) {
@@ -56,36 +30,9 @@ TEST(read_text, read_text_with_trailing_newline) {
 
     std::string path = "../test/read_text_tests_newline_at_end.txt";
     auto context = read_text_context(path);
-    string input = context.read_text();
+    auto container = make_container<character>(context.size);
+    context.read_text(container);
 
-    ASSERT_EQ(input.size(), context.size);
-    ASSERT_EQ(input.size(), size_t(28));
-    ASSERT_EQ(input[0], 'H');
-    ASSERT_EQ(input[1], 'e');
-    ASSERT_EQ(input[2], 'l');
-    ASSERT_EQ(input[3], 'l');
-    ASSERT_EQ(input[4], 'o');
-    ASSERT_EQ(input[5], '!');
-    ASSERT_EQ(input[6], '\n');
-    ASSERT_EQ(input[7], 'r');
-    ASSERT_EQ(input[8], 'e');
-    ASSERT_EQ(input[9], 'a');
-    ASSERT_EQ(input[10], 'd');
-    ASSERT_EQ(input[11], '_');
-    ASSERT_EQ(input[12], 't');
-    ASSERT_EQ(input[13], 'e');
-    ASSERT_EQ(input[14], 'x');
-    ASSERT_EQ(input[15], 't');
-    ASSERT_EQ(input[16], ' ');
-    ASSERT_EQ(input[17], 't');
-    ASSERT_EQ(input[18], 'e');
-    ASSERT_EQ(input[19], 's');
-    ASSERT_EQ(input[20], 't');
-    ASSERT_EQ(input[21], '.');
-    ASSERT_EQ(input[22], '\n');
-    ASSERT_EQ(input[23], 'B');
-    ASSERT_EQ(input[24], 'y');
-    ASSERT_EQ(input[25], 'e');
-    ASSERT_EQ(input[26], '!');
-    ASSERT_EQ(input[22], '\n');
+    ASSERT_EQ(container.size(), size_t(28));
+    ASSERT_EQ(container, "Hello!\nread_text test.\nBye!\n"_s);
 }
