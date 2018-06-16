0f2a5bf7a2e9d8462afa5d7dcd802b0428292ab5
Marvin Löbel <loebel.marvin@gmail.com>
2018-06-16 14:33:43 +0200

    Remove now-redunant using directives in register.cpp

--- sacabench/saca/register.cpp
+++ sacabench/saca/register.cpp
@@ -21,43 +21,18 @@
 
 namespace sacabench::saca {
 
-using deep_shallow = sacabench::deep_shallow::saca;
-SACA_REGISTER(deep_shallow)
-
-using saca_bucket_pointer_refinement =
-    sacabench::bucket_pointer_refinement::bucket_pointer_refinement;
-SACA_REGISTER(saca_bucket_pointer_refinement)
-
-using saca_prefix_doubling = sacabench::prefix_doubling::prefix_doubling;
-SACA_REGISTER(saca_prefix_doubling)
-
-using saca_prefix_doubling_discarding =
-    sacabench::prefix_doubling::prefix_doubling_discarding;
-SACA_REGISTER(saca_prefix_doubling_discarding)
-
-using saca_sais = sacabench::sais::sais;
-SACA_REGISTER(saca_sais)
-
-using saca_gsaca = sacabench::gsaca::gsaca;
-SACA_REGISTER(saca_gsaca)
-
-using saca_dc7 = sacabench::dc7::dc7;
-SACA_REGISTER(saca_dc7)
-
-using saca_qsufsort_naive = sacabench::qsufsort::qsufsort_naive;
-SACA_REGISTER(saca_qsufsort_naive)
-
-using saca_qsufsort = sacabench::qsufsort::qsufsort;
-SACA_REGISTER(saca_qsufsort)
-
-using saca_naive = sacabench::naive::naive;
-SACA_REGISTER(saca_naive)
-
-using sacak = sacabench::sacak::sacak;
-SACA_REGISTER(sacak);
-
-using saca_dc3 = sacabench::dc3::dc3;
-SACA_REGISTER(saca_dc3)
+SACA_REGISTER(sacabench::deep_shallow::saca)
+SACA_REGISTER(sacabench::bucket_pointer_refinement::bucket_pointer_refinement)
+SACA_REGISTER(sacabench::prefix_doubling::prefix_doubling)
+SACA_REGISTER(sacabench::prefix_doubling::prefix_doubling_discarding)
+SACA_REGISTER(sacabench::sais::sais)
+SACA_REGISTER(sacabench::gsaca::gsaca)
+SACA_REGISTER(sacabench::dc7::dc7)
+SACA_REGISTER(sacabench::qsufsort::qsufsort_naive)
+SACA_REGISTER(sacabench::qsufsort::qsufsort)
+SACA_REGISTER(sacabench::naive::naive)
+SACA_REGISTER(sacabench::sacak::sacak)
+SACA_REGISTER(sacabench::dc3::dc3)
 
 } // namespace sacabench::saca
 
