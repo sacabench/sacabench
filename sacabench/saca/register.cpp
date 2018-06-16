e6771d150cd1166d41f41c40947cee23b05fb30f
Marvin Löbel <loebel.marvin@gmail.com>
2018-06-16 14:41:16 +0200

    Group registering and includes together

--- sacabench/saca/register.cpp
+++ sacabench/saca/register.cpp
@@ -4,36 +4,38 @@
  * All rights reserved. Published under the BSD-3 license in the LICENSE file.
  ******************************************************************************/
 
-#include "saca/bucket_pointer_refinement.hpp"
-
-#include "saca/bucket_pointer_refinement.hpp"
-#include "saca/dc3.hpp"
-#include "saca/dc7.hpp"
-#include "saca/deep_shallow/saca.hpp"
-#include "saca/gsaca.hpp"
-#include "saca/naive.hpp"
-#include "saca/prefix_doubling.hpp"
-#include "saca/qsufsort.hpp"
-#include "saca/sacak.hpp"
-#include "saca/sais.hpp"
-
 #include "util/saca.hpp"
 
-namespace sacabench::saca {
-
+#include "saca/deep_shallow/saca.hpp"
 SACA_REGISTER(sacabench::deep_shallow::saca)
+
+#include "saca/bucket_pointer_refinement.hpp"
 SACA_REGISTER(sacabench::bucket_pointer_refinement::bucket_pointer_refinement)
+
+#include "saca/prefix_doubling.hpp"
 SACA_REGISTER(sacabench::prefix_doubling::prefix_doubling)
 SACA_REGISTER(sacabench::prefix_doubling::prefix_doubling_discarding)
+
+#include "saca/sais.hpp"
 SACA_REGISTER(sacabench::sais::sais)
+
+#include "saca/gsaca.hpp"
 SACA_REGISTER(sacabench::gsaca::gsaca)
+
+#include "saca/dc7.hpp"
 SACA_REGISTER(sacabench::dc7::dc7)
+
+#include "saca/qsufsort.hpp"
 SACA_REGISTER(sacabench::qsufsort::qsufsort_naive)
 SACA_REGISTER(sacabench::qsufsort::qsufsort)
-SACA_REGISTER(sacabench::naive::naive)
-SACA_REGISTER(sacabench::sacak::sacak)
-SACA_REGISTER(sacabench::dc3::dc3)
 
-} // namespace sacabench::saca
+#include "saca/naive.hpp"
+SACA_REGISTER(sacabench::naive::naive)
+
+#include "saca/sacak.hpp"
+SACA_REGISTER(sacabench::sacak::sacak)
+
+#include "saca/dc3.hpp"
+SACA_REGISTER(sacabench::dc3::dc3)
 
 /******************************************************************************/
