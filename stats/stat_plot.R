2d825fccdb4676646e935cef587a13cd64b08bb9
Johannes Bahne <johannes.bahne@tu-dortmund.de>
2018-06-22 00:08:52 +0200

    added license header  and plot data from real JSON file with each phase

--- stats/stat_plot.R
+++ stats/stat_plot.R
@@ -1,3 +1,11 @@
+#/*******************************************************************************
+# * Copyright (C) 2018 Rosa Pink <rosa.pink@tu-dortmund.de>
+# * Copyright (C) 2018 Hermann Foot <hermann.foot@tu-dortmund.de>
+# * Copyright (C) 2018 Johannes Bahne <johannes.bahne@tu-dortmund.de>
+# *
+# * All rights reserved. Published under the BSD-3 license in the LICENSE file.
+# ******************************************************************************/
+ 
 library("rjson")
 
 extract_stats <-function(json_name)
@@ -11,25 +19,57 @@ extract_stats <-function(json_name)
   return(list(name=name,time=runtime,memory=mem))
 }
 
-plot_benchmark_single<-function(names, runtimes, mems)
+plot_benchmark_single<-function(algorithm_name, phase_names, runtimes, mems)
 {
   par(mfrow=c(1,2),mai=c(0.5,1,1,1))
-  barplot(as.matrix(runtimes),beside=FALSE,col = 2:(length(runtimes)+1))
+  
+  #Plots for runtimes in each phase
+  barplot(runtimes[1],beside=FALSE,col = 2, ylab = "in seconds")
+  barplot(as.matrix(runtimes[2:length(runtimes)]),beside=FALSE,col = 3:(length(runtimes)+2), add = TRUE)
   title("Runtime", line=1)
-  barplot(mems,col = 2:(length(mems)+1))
+  #legend("right", legend = phase_names, col = 2:(length(runtimes)+1), pch = 15)
+  #TODO: One Legend for both barplots at bottom
+  
+  #Plots for peak memory usage in each phase
+  barplot(mems, beside=TRUE, col = 2:(length(mems)+1), ylab = "in MB")
+  legend("right", legend = phase_names, col = 2:(length(mems)+1), pch = 15)
   title("Memory Usage", line=1)
-  mtext(name, side=3, outer=TRUE, line=-2,cex = 2)
+  
+  #Header for algorithm name
+  mtext(algorithm_name, side=3, outer=TRUE, line=-2,cex = 2)
 }
+
 extract_details<-function(json_name)
 {
   data= fromJSON(file=json_name,simplify = TRUE)
   #Algorithm
   algorithm= data$sub[1][[1]]$sub[[4]]
-  #Phasen
-  phasen= algorithm$sub
+  #Phases
+  phases= algorithm$sub
+  
+  #get main entries
+  algorithm_name = data$stats[[1]]$value
+  max_mem = algorithm$memPeak
+  runtime_overall = algorithm$timeEnd - algorithm$timeStart
+  
+  #get entries for every phase
+  phase_names = phases[[1]]$title
+  phase_runtimes = phases[[1]]$timeEnd-phases[[1]]$timeStart
+  phase_mems  = phases[[1]]$memPeak
+  
+  for(i in 2:length(phases)){
+    phase_names = cbind(phase_names, phases[[i]]$title)
+    phase_runtimes = rbind(phase_runtimes, phases[[i]]$timeEnd-phases[[i]]$timeStart)
+    phase_mems  = rbind(phase_mems, phases[[i]]$memPeak)
+  }
+  
+  phase_names = cbind("overall", phase_names)
+  phase_runtimes = rbind(runtime_overall, phase_runtimes)
+  phase_mems = rbind(max_mem, phase_mems)
+  
+  #plot
+  plot_benchmark_single(algorithm_name, phase_names, phase_runtimes, phase_mems)
 }
-
-
 
 #testList= list(name="a",x=1,y=2)
 #testList2= list(name="b",x=2,y=1)
