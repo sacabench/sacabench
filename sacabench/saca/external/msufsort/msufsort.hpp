f8897d7c7c1a2a32e385ab16277274f5d2dc8469
NicoBertram <nico.bertram@tu-dortmund.de>
2018-07-01 23:06:58 +0200

    msufsort calculates every SA correct except for one

--- sacabench/saca/external/msufsort/msufsort.hpp
+++ sacabench/saca/external/msufsort/msufsort.hpp
@@ -4,9 +4,8 @@
  * All rights reserved. Published under the BSD-3 license in the LICENSE file.
  ******************************************************************************/
 
-#include <MSufSort.hpp>
-#include <util/container.hpp>
-#include <util/signed_size_type.hpp>
+#include <MSufSort.h>
+#include "../external_saca.hpp"
 #include <util/span.hpp>
 #include <util/string.hpp>
 
@@ -17,7 +16,7 @@ using namespace sacabench::util;
 class msufsort {
 public:
     static constexpr size_t EXTRA_SENTINELS = 1;
-    static constexpr char const* NAME = "Reference_MSufSort";
+    static constexpr char const* NAME = "Reference-MSufSort";
     static constexpr char const* DESCRIPTION =
         "Reference MSufSort by M. Maniscalco.";
 
@@ -25,7 +24,28 @@ public:
     static void construct_sa(util::string_span text,
                              sacabench::util::alphabet alphabet,
                              util::span<sa_index> out_sa) {
-        //TODO: Call MSufSort
+        external_saca_with_writable_text<sa_index>(text, out_sa, text.size(), msufsort_ref);
+    }
+    
+private:
+    static void msufsort_ref(unsigned char* text, int32_t* sa, int32_t n) {
+        MSufSort* m_suffixSorter = new MSufSort;
+        m_suffixSorter->Sort(text, n);
+        
+        for (int32_t i = 0; i < n; ++i) {
+            std::cout << m_suffixSorter->ISA(i)-1 << ", ";
+        }
+        std::cout << std::endl;
+        
+        //calculate SA from ISA
+        for (int32_t i = 0; i < n; ++i) {
+            sa[m_suffixSorter->ISA(i)-1] = i;
+        }
+        
+        for (int32_t i = 0; i < n; ++i) {
+            std::cout << sa[i] << ", ";
+        }
+        std::cout << std::endl;
     }
 };
 } // namespace sacabench:reference_sacas
