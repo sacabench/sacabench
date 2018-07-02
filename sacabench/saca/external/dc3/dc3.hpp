c9cabd66e882764a1299e074e60961fa41bf8e2b
David Piper <david.piper@udo.edu>
2018-07-02 13:30:04 +0200

    Improvments on wrapper for dc3.

--- sacabench/saca/external/dc3/dc3.hpp
+++ sacabench/saca/external/dc3/dc3.hpp
@@ -7,7 +7,9 @@
 #pragma once
 
 #include "../external_saca.hpp"
+#include "../../../../external/reference_impls/dc3/drittel.C"
 #include <cstdint>
+#include <util/alphabet.hpp>
 
 namespace sacabench::reference_sacas {
 
@@ -18,12 +20,41 @@ namespace sacabench::reference_sacas {
         static constexpr char const* DESCRIPTION = "Difference Cover Modulo 3 SACA";
 
         template <typename sa_index>
-        inline static void construct_sa(util::string_span text,
-                                        const util::alphabet&,
+        inline static void construct_sa(util::string_span text_with_sentinels,
+                                        const util::alphabet& alphabet,
                                         util::span<sa_index> out_sa) {
 
-            // void suffixArray(int* s, int* SA, int n, int K)
-            // external_saca<sa_index>(text, out_sa, text.size(), ::suffixArray);
+            tdc::StatPhase::pause_tracking();
+
+            int n = text_with_sentinels.size();
+            std::cout << "n: " << n << std::endl;
+            if (n < 4) {
+                return;
+            }
+
+            auto s = std::make_unique<int[]>(n);
+            auto SA = std::make_unique<int[]>(n);
+            int K = alphabet.size_with_sentinel();
+            std::cout << "K: " << K << std::endl;
+
+            for (size_t index = 0; index < n; index++) {
+                s[index] = static_cast<int>(text_with_sentinels[index]);
+            }
+
+            tdc::StatPhase::resume_tracking();
+
+            std::cout << " Started ref " << std::endl;
+            ::suffixArray(s.get(), SA.get(), n, K);
+            std::cout << " Finished ref " << std::endl;
+
+            tdc::StatPhase::pause_tracking();
+
+            for (size_t index = 0; index < n; index++) {
+                out_sa[index] = static_cast<sa_index>(SA[index]);
+                std::cout << out_sa[index] << ", ";
+            }
+            std::cout << std::endl;
+            tdc::StatPhase::resume_tracking();
         }
 
     }; // class dc3
--- sacabench/saca/external/external_saca.hpp
+++ sacabench/saca/external/external_saca.hpp
@@ -59,4 +59,34 @@ inline void external_saca(util::string_span text, util::span<sa_index> out_sa,
     }
     tdc::StatPhase::resume_tracking();
 }
+
+/// \brief Use this if your SACA overwrites the input texts or sentinels,
+///        but uses int32 as the SA index type.
+    template <typename sa_index, typename inner_sa_type, typename Fn>
+    inline void sadslike(util::string_span text, util::span<sa_index> out_sa,
+                         size_t n, size_t alphabet_size, Fn saca_fn) {
+
+        tdc::StatPhase::pause_tracking();
+        auto sa_correct_size = util::make_container<inner_sa_type>(n);
+        util::container<uint8_t> writeable_text(text);
+
+        if (n < 2) {
+            return;
+        }
+        tdc::StatPhase::resume_tracking();
+
+        {
+            saca_fn(writeable_text.data(), sa_correct_size.data(), n, alphabet_size,
+                    n, 0);
+        }
+
+        tdc::StatPhase::pause_tracking();
+        const size_t SENTINELS = text.size() - n;
+
+        for (size_t i = 0; i < n; ++i) {
+            out_sa[SENTINELS + i] = sa_correct_size[i];
+        }
+        tdc::StatPhase::resume_tracking();
+    }
+
 } // namespace sacabench::reference_sacas
