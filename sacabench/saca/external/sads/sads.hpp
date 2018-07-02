b70fd160182e97c6310a9739eb63870edb972959
Marvin Böcker <marvin.boecker@tu-dortmund.de>
2018-07-02 10:51:19 +0200

    remove mergeconflict marker

--- sacabench/saca/external/sads/sads.hpp
+++ sacabench/saca/external/sads/sads.hpp
@@ -14,23 +14,9 @@ public:
     static void construct_sa(util::string_span text,
                              sacabench::util::alphabet alphabet,
                              util::span<sa_index> out_sa) {
-<<<<<<< HEAD
         sadslike<sa_index, int32_t>(
             text, out_sa, text.size(), alphabet.size_with_sentinel(),
             sacabench::reference_sacas::sads_reference::SA_DS);
-=======
-
-        auto SA = std::make_unique<int[]>(text.size());
-
-        if (text.size() > 1) {
-            tdc::StatPhase sads("Main Phase");
-            sacabench::reference_sacas::sads_reference::SA_DS(text.data(), SA.get(), text.size(), alphabet.max_character_value() + 1, text.size(), 0);
-        }
-        for (size_t i = 0; i < text.size(); i++) {
-            DCHECK_LE(static_cast<size_t>(SA[i]), std::numeric_limits<sa_index>::max());
-            out_sa[i] = static_cast<sa_index>(SA[i]);
-        }
->>>>>>> origin/master
     }
 };
 } // namespace sacabench::reference_sacas
