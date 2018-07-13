345de23f3737945b32892bcb8b9fded9f2392845
Florian Grieskamp <f.grieskamp@gmail.com>
2018-07-13 15:12:22 +0200

    Fix equal bptr values with extra sentinel

--- sacabench/saca/bucket_pointer_refinement.hpp
+++ sacabench/saca/bucket_pointer_refinement.hpp
@@ -25,7 +25,7 @@ using namespace sacabench::bucket_pointer_refinement::sort;
 
 class bucket_pointer_refinement {
 public:
-    static constexpr size_t EXTRA_SENTINELS = 0;
+    static constexpr size_t EXTRA_SENTINELS = 1;
     static constexpr char const* NAME = "BPR";
     static constexpr char const* DESCRIPTION =
         "Bucket-Pointer Refinement according to Klaus-Bernd Schürmann";
