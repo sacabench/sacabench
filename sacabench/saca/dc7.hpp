11952a84a6d6ae2f80d1f7c96f039300c1ca134d
Johannes Bahne <johannes.bahne@tu-dortmund.de>
2018-07-14 15:23:01 +0200

    better memory and runtime of DC7.

--- sacabench/saca/dc7.hpp
+++ sacabench/saca/dc7.hpp
@@ -21,7 +21,7 @@ class dc7 {
 public:
     static constexpr size_t EXTRA_SENTINELS = 7;
     static constexpr char const* NAME = "DC7";
-    static constexpr char const* DESCRIPTION = "Difference Cover Modulo 7 SACA";
+    static constexpr char const* DESCRIPTION = "Difference Cover Modulo 7 as described by Juha Kärkkäinen and Peter Sanders";
 
     template <typename sa_index>
     static void construct_sa(util::string_span text,
@@ -53,7 +53,7 @@ private:
 
                 size_t counter = 0;
 
-                for (size_t i = 1; i < n; i++) {
+                for (size_t i = 1; i < n; ++i) {
                     if (((i % 7) == 1) || ((i % 7) == 2) || ((i % 7) == 4)) {
                         tuples_124_to_be_sorted[counter++] =
                             std::tuple<C, C, C, C, C, C, C, size_t>(
@@ -69,7 +69,7 @@ private:
                 std::sort(tuples_124_to_be_sorted.begin(),
                           tuples_124_to_be_sorted.end());
 
-                for (size_t i = 0; i < tuples_124_to_be_sorted.size(); i++) {
+                for (size_t i = 0; i < tuples_124_to_be_sorted.size(); ++i) {
                     tuples_124[i] = std::get<7>(tuples_124_to_be_sorted[i]);
                 }
                 (void)alphabet_size;
@@ -105,7 +105,7 @@ private:
         size_t leq_name = 1;
 
         size_t pos_to_store_leq;
-        for (size_t i = 0; i < tuples_124.size(); i++) {
+        for (size_t i = 0; i < tuples_124.size(); ++i) {
             // set the lexicographical names at correct positions:
             //[----names at positions i mod 7 = 1----||
             // ----names at positions i mod 7 = 2----||
@@ -123,7 +123,7 @@ private:
                 if (sacabench::util::span(&INPUT_STRING[tuples_124[i]], 7) !=
                     sacabench::util::span(&INPUT_STRING[tuples_124[i + 1]],
                                           7)) {
-                    leq_name++;
+                    ++leq_name;
                 } else { // if lexicographical names are not uniqe set recursion
                          // = true
                     recursion = true;
@@ -134,12 +134,11 @@ private:
     }
 
     template <typename S, typename I>
-    static void determine_isa(const S& t_124, I& isa_124) {
+    static void determine_isa(const S& t_124, I& isa_124, u_int8_t modulo) {
         DCHECK_MSG(isa_124.size() == t_124.size(),
                    "isa_124 must have the same length as t_124");
-
-        for (size_t i = 0; i < t_124.size(); i++) {
-            isa_124[t_124[i] - 1] = i + 1;
+        for (size_t i = 0; i < t_124.size(); ++i) {
+            isa_124[(t_124[i] - modulo)/7] = i + 1;
         }
     }
 
@@ -165,10 +164,9 @@ private:
      */
     static void construct_sa_dc7(T& text, util::span<sa_index> out_sa,
                                  size_t alphabet_size) {
-        
         //------------------------Phase 1----------------------//
         tdc::StatPhase dc7("Phase 1");
-
+        
         const size_t n = text.size() - 6;
 
         // empty container which will contain indices of triplet
@@ -222,7 +220,7 @@ private:
             determine_sa(sa_124, isa_124);
 
             // correct the order of sa_124 with result of recursion
-            for (size_t i = 0; i < tuples_124.size(); i++) {
+            for (size_t i = 0; i < tuples_124.size(); ++i) {
                 if (i < start_of_pos_2) {
                     tuples_124[isa_124[i] - 1] = 7 * i + 1;
                 } else if (i < start_of_pos_4) {
@@ -248,17 +246,16 @@ private:
         // empty sa_6 which should be filled correctly with method induce_sa_dc
         auto sa_6 = sacabench::util::make_container<size_t>(length_t_6);
 
-        //----------------------------funktioniert--------------------------------
         if (recursion) {
             // fill sa_3 by inducing with characters at i mod 7 = 3 and ranks of
             // tupels beginning in positions i mod 7 = 2
             // start_pos: position, of ranks i mod 7 = 2 of t_124
-            induce_sa_dc<C>(text, isa_124, sa_3, start_of_pos_2 + 1, 3, n);
+            induce_sa<C>(text, isa_124, sa_3, start_of_pos_2 + 1, 3, n);
 
             // fill sa_5 by inducing with characters at i mod 7 = 5 and ranks of
             // tupels beginning in positions i mod 7 = 4
             // start_pos: position, of ranks i mod 7 = 4 of t_124
-            induce_sa_dc<C>(text, isa_124, sa_5, start_of_pos_4 + 1, 5, n);
+            induce_sa<C>(text, isa_124, sa_5, start_of_pos_4 + 1, 5, n);
 
             // fill sa_6 by inducing with characters at i mod 7 = 6 and ranks of
             // tupels beginning in positions i mod 7 = 5
@@ -267,137 +264,43 @@ private:
             size_t length_t = length_t_5>=length_t_6 ? length_t_5 : length_t_6;
             auto isa = sacabench::util::make_container<size_t>(length_t);
             util::span<size_t> span_isa = util::span(&isa[0],length_t_5);
-
-            determine_sa(sa_5, span_isa);
+            
+            determine_isa(sa_5, span_isa, 5);
             u_int8_t start_pos = 1;
-            induce_sa_dc<C>(text, span_isa, sa_6, start_pos, 6, n);
+            induce_sa<C>(text, span_isa, sa_6, start_pos, 6, n);
             
             // fill sa_0 by inducing with characters at i mod 7 = 0 and ranks of
             // tupels beginning in positions i mod 7 = 6
             span_isa = util::span(&isa[0],length_t_6);
-            determine_sa(sa_6, span_isa);
+            determine_isa(sa_6, span_isa, 6);
             start_pos = 0;
-            induce_sa_dc<C>(text, span_isa, sa_0, start_pos, 0, n);
+            induce_sa<C>(text, span_isa, sa_0, start_pos, 0, n);
         } else {
-            induce_sa_dc<C>(text, span_t_124, sa_3, start_of_pos_2 + 1, 3, n);
+            induce_sa<C>(text, span_t_124, sa_3, start_of_pos_2 + 1, 3, n);
 
             // fill sa_5 by inducing with characters at i mod 7 = 5 and ranks of
             // tupels beginning in positions i mod 7 = 4
             // start_pos: position, of ranks i mod 7 = 4 of t_124
-            induce_sa_dc<C>(text, span_t_124, sa_5, start_of_pos_4 + 1, 5, n);
+            induce_sa<C>(text, span_t_124, sa_5, start_of_pos_4 + 1, 5, n);
 
             // fill sa_6 by inducing with characters at i mod 7 = 6 and ranks of
             // tupels beginning in positions i mod 7 = 5
             
-            // empty isa_5 which should be filled correctly with method induce_sa_dc
+            // empty isa_5 which should be filled correctly with method induce_sa
             size_t length_t = length_t_5>=length_t_6 ? length_t_5 : length_t_6;
             auto isa = sacabench::util::make_container<size_t>(length_t);
             util::span<size_t> span_isa = util::span(&isa[0],length_t_5);
-            determine_sa(sa_5, span_isa);
+            determine_isa(sa_5, span_isa, 5);
             u_int8_t start_pos = 1;
-            induce_sa_dc<C>(text, span_isa, sa_6, start_pos, 6, n);
+            induce_sa<C>(text, span_isa, sa_6, start_pos, 6, n);
             
             // fill sa_0 by inducing with characters at i mod 7 = 0 and ranks of
             // tupels beginning in positions i mod 7 = 6
             span_isa = util::span(&isa[0],length_t_6);
-            determine_sa(sa_6, span_isa);
+            determine_isa(sa_6, span_isa, 6);
             start_pos = 0;
-            induce_sa_dc<C>(text, span_isa, sa_0, start_pos, 0, n);
+            induce_sa<C>(text, span_isa, sa_0, start_pos, 0, n);
         }
-        
-        // rename to correct positions of input text
-        for (size_t i = 0; i < sa_0.size(); i++) {
-            sa_0[i] = sa_0[i] * 7 + 0;
-        }
-        for (size_t i = 0; i < sa_3.size(); i++) {
-            sa_3[i] = sa_3[i] * 7 + 3;
-        }
-        for (size_t i = 0; i < sa_5.size(); i++) {
-            sa_5[i] = sa_5[i] * 7 + 5;
-        }
-        for (size_t i = 0; i < sa_6.size(); i++) {
-            sa_6[i] = sa_6[i] * 7 + 6;
-        }
-        //----------------------------funktioniert--------------------------------
-
-        /*
-        //----------------------------funktioniert nicht--------------------------
-        if (recursion) {
-            // fill sa_3 by inducing with characters at i mod 7 = 3 and ranks of
-            // tupels beginning in positions i mod 7 = 2
-            // start_pos: position, of ranks i mod 7 = 2 of t_124
-            induce_sa_dc_2<C>(text, isa_124, sa_3, start_of_pos_2 + 1, 3, n);
-
-            // fill sa_5 by inducing with characters at i mod 7 = 5 and ranks of
-            // tupels beginning in positions i mod 7 = 4
-            // start_pos: position, of ranks i mod 7 = 4 of t_124
-            induce_sa_dc_2<C>(text, isa_124, sa_5, start_of_pos_4 + 1, 5, n);
-
-            // fill sa_6 by inducing with characters at i mod 7 = 6 and ranks of
-            // tupels beginning in positions i mod 7 = 5
-            
-            // empty isa which should be filled correctly with method induce_sa_dc
-            size_t length_t = length_t_5>=length_t_6 ? length_t_5 : length_t_6;
-            auto isa = sacabench::util::make_container<size_t>(length_t);
-            util::span<size_t> span_isa = util::span(&isa[0],length_t_5);
-
-            determine_sa(sa_5, span_isa);
-            u_int8_t start_pos = 1;
-            induce_sa_dc_2<C>(text, span_isa, sa_6, start_pos, 6, n);
-            
-            // fill sa_0 by inducing with characters at i mod 7 = 0 and ranks of
-            // tupels beginning in positions i mod 7 = 6
-            span_isa = util::span(&isa[0],length_t_6);
-            determine_sa(sa_6, span_isa);
-            start_pos = 0;
-            induce_sa_dc_2<C>(text, span_isa, sa_0, start_pos, 0, n);
-        } else {
-            induce_sa_dc_2<C>(text, span_t_124, sa_3, start_of_pos_2 + 1, 3, n);
-
-            // fill sa_5 by inducing with characters at i mod 7 = 5 and ranks of
-            // tupels beginning in positions i mod 7 = 4
-            // start_pos: position, of ranks i mod 7 = 4 of t_124
-            induce_sa_dc_2<C>(text, span_t_124, sa_5, start_of_pos_4 + 1, 5, n);
-
-            // fill sa_6 by inducing with characters at i mod 7 = 6 and ranks of
-            // tupels beginning in positions i mod 7 = 5
-            
-            // empty isa_5 which should be filled correctly with method induce_sa_dc_2
-            size_t length_t = length_t_5>=length_t_6 ? length_t_5 : length_t_6;
-            auto isa = sacabench::util::make_container<size_t>(length_t);
-            util::span<size_t> span_isa = util::span(&isa[0],length_t_5);
-            determine_sa(sa_5, span_isa);
-            u_int8_t start_pos = 1;
-            induce_sa_dc_2<C>(text, span_isa, sa_6, start_pos, 6, n);
-            
-            // fill sa_0 by inducing with characters at i mod 7 = 0 and ranks of
-            // tupels beginning in positions i mod 7 = 6
-            span_isa = util::span(&isa[0],length_t_6);
-            determine_sa(sa_6, span_isa);
-            start_pos = 0;
-            induce_sa_dc_2<C>(text, span_isa, sa_0, start_pos, 0, n);
-        }
-        //----------------------------funktioniert nicht--------------------------
-        */
-        
-        // rename to correct positions of input text
-        for (size_t i = 0; i < sa_0.size(); i++) {
-            std::cout << sa_0[i] << ", ";
-        }
-        
-        std::cout << std::endl;
-        for (size_t i = 0; i < sa_3.size(); i++) {
-            std::cout << sa_3[i] << ", ";
-        }
-        std::cout << std::endl;
-        for (size_t i = 0; i < sa_5.size(); i++) {
-            std::cout << sa_5[i] << ", ";
-        }
-        std::cout << std::endl;
-        for (size_t i = 0; i < sa_6.size(); i++) {
-            std::cout << sa_6[i] << ", ";
-        }
-        std::cout << std::endl;
         
         //-----------------------Phase 3----------------------//
         dc7.split("Phase 3");
@@ -406,66 +309,34 @@ private:
         // 3 = 0, 3, 5, 6
         if constexpr (rec) {
             if (recursion) {
-                merge_sa_dc<size_t>(text, sa_0, tuples_124, sa_3, sa_5, sa_6,
+                merge_sa<size_t>(text, sa_0, tuples_124, sa_3, sa_5, sa_6,
                                     isa_124, start_of_pos_2, start_of_pos_4,
                                     out_sa);
             } else {
-                merge_sa_dc<size_t>(text, sa_0, tuples_124, sa_3, sa_5, sa_6,
+                merge_sa<size_t>(text, sa_0, tuples_124, sa_3, sa_5, sa_6,
                                     span_t_124, start_of_pos_2, start_of_pos_4,
                                     out_sa);
             }
 
         } else {
             if (recursion) {
-                merge_sa_dc<sacabench::util::character>(
+                merge_sa<sacabench::util::character>(
                     text, sa_0, tuples_124, sa_3, sa_5, sa_6, isa_124,
                     start_of_pos_2, start_of_pos_4, out_sa);
             } else {
-                merge_sa_dc<sacabench::util::character>(
+                merge_sa<sacabench::util::character>(
                     text, sa_0, tuples_124, sa_3, sa_5, sa_6, span_t_124,
                     start_of_pos_2, start_of_pos_4, out_sa);
             }
         }
     }
-    template <typename C, typename T, typename I, typename S>
-    static void induce_sa_dc(const T& input_text, const I& isa,
-                             S& sa, const size_t& start_pos, const size_t& modulo, const size_t& length) {
-
-        // Container to store all tuples with the same length as sa
-        // Tuples contains six characters, a rank and the start position
-        auto sa_to_be_sorted = sacabench::util::make_container<
-            std::tuple<C, C, C, C, C, C, size_t, size_t>>(sa.size());
-
-        for (size_t i = modulo; i < length; i += 7) {
-            if ((start_pos + i/7) < isa.size()) {
-                sa_to_be_sorted[i/7] =
-                    (std::tuple<C, C, C, C, C, C, size_t, size_t>(
-                        input_text[i], input_text[i + 1],
-                        input_text[i + 2], input_text[i + 3],
-                        input_text[i + 4], input_text[i + 5],
-                        isa[start_pos + i/7], i/7));
-            } else {
-                sa_to_be_sorted[i/7] =
-                    (std::tuple<C, C, C, C, C, C, size_t, size_t>(
-                        input_text[i], input_text[i + 1],
-                        input_text[i + 2], input_text[i + 3],
-                        input_text[i + 4], input_text[i + 5], 0, i/7));
-            }
-        }
-
-        // TODO: sort Tupels with radix_sort
-        // radix_sort(sa0_to_be_sorted, sa0);
-
-        std::sort(sa_to_be_sorted.begin(), sa_to_be_sorted.end());
-        for (size_t i = 0; i < sa_to_be_sorted.size(); i++) {
-            sa[i] = std::get<7>(sa_to_be_sorted[i]);
-        }
-    }
     
     template <typename C, typename T, typename I, typename S>
-    static void induce_sa_dc_2(const T& input_text, const I& isa,
+    static void induce_sa(const T& input_text, const I& isa,
                              S& sa, const size_t& start_pos, const size_t& modulo, const size_t& length) {
 
+        DCHECK_MSG(isa.size() == sa.size(),
+                   "isa must have the same length as sa");
         // Container to store all tuples with the same length as sa
         // Tuples contains six characters, a rank and the start position
         auto sa_to_be_sorted = sacabench::util::make_container<
@@ -492,14 +363,14 @@ private:
         // radix_sort(sa0_to_be_sorted, sa0);
 
         std::sort(sa_to_be_sorted.begin(), sa_to_be_sorted.end());
-        for (size_t i = 0; i < sa_to_be_sorted.size(); i++) {
+        for (size_t i = 0; i < sa_to_be_sorted.size(); ++i) {
             sa[i] = std::get<7>(sa_to_be_sorted[i]);
         }
     }
 
     template <typename C, typename T, typename S, typename S_124, typename I,
               typename SA>
-    static void merge_sa_dc(const T& text, const S& sa_0, const S_124& sa_124,
+    static void merge_sa(const T& text, const S& sa_0, const S_124& sa_124,
                             const S& sa_3, const S& sa_5, const S& sa_6,
                             const I& isa_124, const size_t start_of_pos_2,
                             const size_t start_of_pos_4, SA& sa) {
@@ -536,25 +407,30 @@ private:
             (std::tuple<size_t, size_t, size_t, size_t, size_t, size_t, size_t>(
                 2, 3, 2, 5, 5, 3, 0));
 
-        // Store all SAs to a container to get better access
-        auto all_sa = sacabench::util::make_container<std::vector<size_t>>(5);
-
-        for (size_t i = 0; i < sa_0.size(); ++i) {
-            all_sa[0].push_back(sa_0[i]);
+        // init an array of spans to all SAs to get better access
+        sacabench::util::span<const size_t> all_sa[5];
+        if(sa_0.size() != 0){
+            sacabench::util::span<const size_t> span_sa_0 = sacabench::util::span(&sa_0[0], sa_0.size());
+            all_sa[0] = span_sa_0;
         }
-        for (size_t i = 0; i < sa_124.size(); ++i) {
-            all_sa[1].push_back(sa_124[i]);
+        if(sa_124.size() != 0){
+            sacabench::util::span<const size_t> span_sa_124 = sacabench::util::span(&sa_124[0], sa_124.size());
+            all_sa[1] = span_sa_124;
         }
-        for (size_t i = 0; i < sa_3.size(); ++i) {
-            all_sa[2].push_back(sa_3[i]);
+        if(sa_3.size() != 0){
+            sacabench::util::span<const size_t> span_sa_3 = sacabench::util::span(&sa_3[0], sa_3.size());
+            all_sa[2] = span_sa_3;
         }
-        for (size_t i = 0; i < sa_5.size(); ++i) {
-            all_sa[3].push_back(sa_5[i]);
+        if(sa_5.size() != 0){
+            sacabench::util::span<const size_t> span_sa_5 = sacabench::util::span(&sa_5[0], sa_5.size());
+            all_sa[3] = span_sa_5;
         }
-        for (size_t i = 0; i < sa_6.size(); ++i) {
-            all_sa[4].push_back(sa_6[i]);
+        if(sa_6.size() != 0){
+            sacabench::util::span<const size_t> span_sa_6 = sacabench::util::span(&sa_6[0], sa_6.size());
+            all_sa[4] = span_sa_6;
         }
-
+        
+        
         // counters to know, which elements will be compared
         std::array<size_t, 5> counters = {0, 0, 0, 0, 0};
         // store length of all SAs
@@ -562,7 +438,7 @@ private:
         for (size_t i = 0; i < max_counters.size(); ++i) {
             max_counters[i] = all_sa[i].size();
         }
-
+    
         switch (text.size() % 7) {
         case 0:
             ++counters[0];
@@ -586,15 +462,15 @@ private:
             ++counters[4];
             break;
         }
-
+        
         // queue to know which SAs will be compared
         // 0 = 0, 1 = 124, 2 = 3, 3 = 4, 4 = 5
-        std::queue<size_t> queue;
+        std::queue<u_int8_t> queue;
 
         // container to compare the two compared SAs
         auto to_be_compared =
             sacabench::util::make_container<std::vector<C>>(2);
-
+            
         // Loop until the SA is filled
         for (size_t sa_ind = 0; sa_ind < sa.size(); ++sa_ind) {
             // fill the queue with SA-numbers, which are not yet compared
@@ -605,9 +481,10 @@ private:
                 }
             }
 
-            size_t comp_1;
-            size_t comp_2;
-            size_t smallest = queue.front();
+            u_int8_t comp_1;
+            u_int8_t comp_2;
+            u_int8_t smallest = queue.front();
+            
             while (queue.size() > 1) {
 
                 // get the first two elements of the queue and pop them.
