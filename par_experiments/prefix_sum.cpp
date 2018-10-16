8edf7a1163aa9314ee2b487cdd7823371201a9b0
magiera <oliver.magiera@tu-dortmund.de>
2018-10-16 17:27:21 +0200

    Added heap-like index calculation for container.

--- par_experiments/prefix_sum.cpp
+++ par_experiments/prefix_sum.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+
+struct node {
+    size_t from_left;
+    size_t sum;
+}
+
+// Needed for down pass
+size_t left_child(int parent) { return 2*parent + 1; }
+
+size_t right_child(int parent) { return 2*parent+2; }
+
+
+// Needed for up pass
+size_t parent_from_left_child(int child) { return (parent-1)/2; }
+
+size_t parent_from_right_child(int child) { return (parent-2)/2; }
