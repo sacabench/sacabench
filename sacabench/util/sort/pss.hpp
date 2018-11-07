3bfbe6ac1f44b6aa708767ca11ef747623a4abd2
David Piper <david.piper@tu-dortmund.de>
2018-11-07 11:44:05 +0100

    Added wrapper for pss and test case.

---
+++ sacabench/util/sort/pss.hpp
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (C) 2018 Florian Grieskamp <florian.grieskamp@tu-dortmund.de>
+ *                    David Piper <david.piper@tu-dortmund.de>
+ *                    Marvin Böcker <marvin.boecker@tu-dortmund.de>
+ *
+ * All rights reserved. Published under the BSD-3 license in the LICENSE file.
+ ******************************************************************************/
+
+#pragma once
+
+#include <algorithm>
+
+#include "util/container.hpp"
+#include "util/span.hpp"
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#define USE_OPENMP
+#include "../external/pss/parallel_stable_sort.h"
+#pragma GCC diagnostic pop
+
+namespace sacabench::util::sort {
+
+template <typename T, typename Compare>
+void pss_sort(container<T>& data, Compare comp) {
+    pss::parallel_stable_sort(std::begin(data), std::end(data), comp);
+}
+/*
+template <typename T, typename Compare>
+void ips4o_sort(span<T> data, Compare comp) {
+    ips4o::sort(std::begin(data), std::end(data), comp);
+}
+
+template <typename T, typename Compare>
+void ips4o_sort_parallel(container<T>& data, Compare comp) {
+    ips4o::parallel::sort(std::begin(data), std::end(data), comp);
+}
+
+template <typename T, typename Compare>
+void ips4o_sort_parallel(span<T> data, Compare comp) {
+    ips4o::parallel::sort(std::begin(data), std::end(data), comp);
+}*/
+
+} // namespace sacabench::util::sort
+
+/******************************************************************************/
---
+++ test/pss_tests.cpp
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (C) 2018 Florian Grieskamp <florian.grieskamp@tu-dortmund.de>
+ *                    David Piper <david.piper@tu-dortmund.de>
+ *                    Marvin Böcker <marvin.boecker@tu-dortmund.de>
+ *
+ * All rights reserved. Published under the BSD-3 license in the LICENSE file.
+ ******************************************************************************/
+
+#include <gtest/gtest.h>
+#include <util/sort/pss.hpp>
+#include <util/container.hpp>
+
+using namespace sacabench::util;
+
+TEST(pss_tests, sort_correct) {
+    container<int> test_inst = {3,1,9,6,4,2,7,8,5};
+    container<int> control_inst = {1,2,3,4,5,6,7,8,9};
+    sort::pss_sort<int>(test_inst, std::less<int>());
+    ASSERT_EQ(test_inst, control_inst);
+}
