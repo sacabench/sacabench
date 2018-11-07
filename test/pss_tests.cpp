b6c0b949058b146500cce6d096942ea4ab7a2e53
Florian Grieskamp <f.grieskamp@gmail.com>
2018-11-07 13:28:34 +0100

    Prepare for long tests

--- test/pss_tests.cpp
+++ test/pss_tests.cpp
@@ -15,21 +15,47 @@ using namespace sacabench::util;
 
 class pss_tests : public ::testing::Test {
     protected:
-        void SetUp() override {
-            test_inst = {3,1,9,6,4,2,7,8,5};
-            control_inst = {1,2,3,4,5,6,7,8,9};
+        static void SetUpTestCase() {
+            short_inst = {3,1,9,6,4,2,7,8,5};
+            short_control_inst = {1,2,3,4,5,6,7,8,9};
+            long_inst = {3,1,9,6,4,2,7,8,5};
+            long_control_inst = {1,2,3,4,5,6,7,8,9};
         }
 
-        container<int> test_inst;
-        container<int> control_inst;
+        void SetUp() override {
+            short_test_inst = container<int>(short_inst);
+            long_test_inst = container<int>(long_inst);
+        }
+
+        static container<int> short_inst;
+        static container<int> short_control_inst;
+        static container<int> long_inst;
+        static container<int> long_control_inst;
+        container<int> short_test_inst;
+        container<int> long_test_inst;
 };
 
+container<int> pss_tests::short_inst;
+container<int> pss_tests::long_inst;
+container<int> pss_tests::short_control_inst;
+container<int> pss_tests::long_control_inst;
+
 TEST_F(pss_tests, intel_sort_correct) {
-    sort::parallel_stable<int>(test_inst, std::less<int>());
-    ASSERT_EQ(test_inst, control_inst);
+    sort::parallel_stable<int>(short_test_inst, std::less<int>());
+    ASSERT_EQ(short_test_inst, short_control_inst);
 }
 
 TEST_F(pss_tests, std_sort_correct) {
-    __gnu_parallel::stable_sort(std::begin(test_inst), std::end(test_inst), std::less<int>());
-    ASSERT_EQ(test_inst, control_inst);
+    __gnu_parallel::stable_sort(std::begin(short_test_inst), std::end(short_test_inst), std::less<int>());
+    ASSERT_EQ(short_test_inst, short_control_inst);
+}
+
+TEST_F(pss_tests, intel_sort_long) {
+    sort::parallel_stable<int>(short_test_inst, std::less<int>());
+    ASSERT_EQ(short_test_inst, short_control_inst);
+}
+
+TEST_F(pss_tests, std_sort_long) {
+    __gnu_parallel::stable_sort(std::begin(long_test_inst), std::end(long_test_inst), std::less<int>());
+    ASSERT_EQ(long_test_inst, long_control_inst);
 }
