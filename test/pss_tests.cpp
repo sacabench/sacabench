1a11da9f266d672b9f7dc60246f7f71ea70564bd
Florian Grieskamp <f.grieskamp@gmail.com>
2018-11-07 12:55:13 +0100

    Add test for parallel std::stable_sort

--- test/pss_tests.cpp
+++ test/pss_tests.cpp
@@ -9,12 +9,20 @@
 #include <gtest/gtest.h>
 #include <util/sort/pss.hpp>
 #include <util/container.hpp>
+#include <parallel/algorithm>
 
 using namespace sacabench::util;
 
-TEST(pss_tests, sort_correct) {
+TEST(pss_tests, intel_sort_correct) {
     container<int> test_inst = {3,1,9,6,4,2,7,8,5};
     container<int> control_inst = {1,2,3,4,5,6,7,8,9};
     sort::parallel_stable<int>(test_inst, std::less<int>());
+    ASSERT_EQ(test_inst, control_inst);
+}
+
+TEST(pss_tests, std_sort_correct) {
+    container<int> test_inst = {3,1,9,6,4,2,7,8,5};
+    container<int> control_inst = {1,2,3,4,5,6,7,8,9};
+    __gnu_parallel::stable_sort(std::begin(test_inst), std::end(test_inst), std::less<int>());
     ASSERT_EQ(test_inst, control_inst);
 }
