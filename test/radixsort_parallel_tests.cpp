edb9af1a3b79e55e25dbdc4732f792f817e8a549
David Piper <david.piper@udo.edu>
2018-11-18 17:45:59 +0100

    Added test case for many random elements as input.

--- test/radixsort_parallel_tests.cpp
+++ test/radixsort_parallel_tests.cpp
@@ -7,6 +7,7 @@
 #include <gtest/gtest.h>
 #include <util/sort/radixsort_parallel.hpp>
 #include <util/container.hpp>
+#include <random>
 
 using namespace sacabench::util;
 
@@ -35,9 +36,19 @@ TEST(radixsort_parallel_tests, element_number_regardless_of_thred_count) {
 }
 
 TEST(radixsort_parallel_tests, big_random_test) {
-    container<int> test_input = {123, 223, 912, 691, 735};
-    container<int> test_output = make_container<int>(5);
-    container<int> test_control = {123, 223, 691, 735, 912};
+
+    // https://stackoverflow.com/a/32887614
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(100, 999);
+
+    container<int> test_input(1000000);
+    std::generate(test_input.begin(), test_input.end(), [&] { return distribution(generator); });
+
+    container<int> test_output = make_container<int>(1000000);
+    
     sort::radixsort_parallel(test_input, test_output);
-    ASSERT_EQ(test_output, test_control);
+    
+    for (int index = 0; index < test_input.size() - 1; index++) {
+        ASSERT_LE(test_output[index], test_output[index + 1]);
+    }
 }
