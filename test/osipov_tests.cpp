0ab3a3372add96919f83d6634b6af0b3016fcb8c
Florian Grieskamp <f.grieskamp@gmail.com>
2018-11-26 10:44:58 +0100

    Make tests great again

--- test/osipov_tests.cpp
+++ test/osipov_tests.cpp
@@ -4,16 +4,16 @@
 #include <util/container.hpp>
 #include <util/span.hpp>
 #include <util/string.hpp>
-#include <saca/prefix_doubling_osipov.hpp>
+#include <saca/osipov/osipov_sequential.hpp>
 #include "test/saca.hpp"
 
 using namespace sacabench::osipov;
 using namespace sacabench;
 
 TEST(Osipov, CornerCases) {
-    test::saca_corner_cases<sacabench::osipov::osipov>();
+    test::saca_corner_cases<sacabench::osipov::osipov_sequential>();
 }
 
 TEST(OsipovWp, CornerCases) {
-    test::saca_corner_cases<sacabench::osipov::osipov_wp>();
+    test::saca_corner_cases<sacabench::osipov::osipov_sequential_wp>();
 }
