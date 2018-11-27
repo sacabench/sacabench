f084c11e051d1dfc5a683c41a1f69c7ddb0c2869
magiera <oliver.magiera@tu-dortmund.de>
2018-11-27 13:10:30 +0100

    Fixed error for inclusive prefix sum (not using sum operator). Added test for max operator.

--- sacabench/util/prefix_sum.hpp
+++ sacabench/util/prefix_sum.hpp
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (C) 2018 Marvin Böcker <marvin.boecker@udo.edu>
+ * Copyright (C) 2018 Oliver Magiera <oliver.magiera@tu-dortmund.de>
+ * Copyright (C) 2018 Rosa Pink <rosa.pink@tu-dortmund.de>
+ *
+ * All rights reserved. Published under the BSD-3 license in the LICENSE file.
+ ******************************************************************************/
+
+
 #include <iostream>
 #include <cstring>
 #include <omp.h>
@@ -129,7 +138,7 @@ void par_prefix_sum(span<Content> in, span<Content> out, bool inclusive,
     if(inclusive) {
         #pragma omp simd
         for(size_t i = 0; i < in.size(); ++i) {
-            out[i] = in[i] + tree[tree_size + i];
+            out[i] = add(in[i], tree[tree_size + i]);
         }
     } else {
         #pragma omp simd
@@ -137,122 +146,6 @@ void par_prefix_sum(span<Content> in, span<Content> out, bool inclusive,
             out[i] = tree[tree_size + i];
         }
     }
-}
-
-
-// Parallel version of the prefix sum calculation.
-template<typename Content>
-void par_prefix_sum(const Content* __restrict__ in, const size_t len, Content* __restrict__ out) {
-    // Correct length for odd lengths.
-    const size_t corrected_len = (len % 2 == 1) ? len + 1 : len;
-    const size_t tree_size = next_power_of_two(len);
-
-    // Create tree structure
-    auto tree = new Content[tree_size + corrected_len];
-    tree[0] = 42; // lol
-
-    // Copy input into tree leafs
-    memcpy(tree + tree_size, in, len * sizeof(Content));
-
-    // ########################################################
-    // UP PASS
-    // ########################################################
-
-    #pragma omp parallel
-    {
-        for(size_t offset = tree_size; offset != 1; offset /= 2) {
-
-            #pragma omp for
-            for(size_t i = 0; i < std::min(offset, corrected_len); i += 2) {
-                const size_t j = offset + i;
-                const size_t k = offset + i + 1;
-                tree[parent_of_child(j)] = tree[j] + tree[k];
-            }
-        }
-
-        tree[1] = 0;
-
-        for(size_t offset = 1; offset != tree_size; offset *= 2) {
-
-            const size_t layer_size = offset == tree_size / 2 ? corrected_len / 2 : offset;
-
-            #pragma omp for
-            for(size_t i = 0; i < layer_size; i++) {
-                const size_t j = layer_size - i - 1;
-
-                const Content& from_left = tree[offset + j];
-                const Content& left_sum = tree[left_child(offset + j)];
-                tree[right_child(offset + j)] = from_left + left_sum;
-                tree[left_child(offset + j)] = from_left;
-            }
-        }
-
-        // ########################################################
-        // FINAL PASS
-        // ########################################################
-
-        #pragma omp simd
-        for(size_t i = 0; i < len; ++i) {
-            out[i] = in[i] + tree[tree_size + i];
-        }
-    }
-}
-
-int main() {
-
-  srand(time(NULL));
-
-  // Number of CPUs.
-  //const size_t n_cpus = std::thread::hardware_concurrency();
-  //omp_set_num_threads(n_cpus);
-
-  // Calc prefixsum with threads.
-  const size_t len = 36;
-  const size_t test_data_len = len*1024*1024;
-
-  std::cout << "memory: " << (test_data_len * sizeof(size_t) / 1024.0 / 1024.0) << "MiB" << std::endl;
-
-  // Alloc memory.
-  auto container = make_container<size_t>(test_data_len);
-  auto container2 = make_container<size_t>(test_data_len);
-  auto container3 = make_container<size_t>(test_data_len);
-  auto ptr = container.begin();
-  auto ptr2 = container2.begin();
-  auto ptr3 = container3.begin();
-
-  for(size_t i = 0; i < test_data_len; ++i) {
-  	ptr[i] = rand();
-  }
-
-  std::cout << "random generation done" << std::endl;
-
-  //print_arr(ptr, test_data_len);
-
-  auto startt = std::chrono::steady_clock::now();
-
-  seq_prefix_sum<size_t, sum<size_t>>(span<size_t>(container), span<size_t>(container3), true, sum<size_t>(), 0);;
-
-  auto midt = std::chrono::steady_clock::now();
-
-  par_prefix_sum<size_t, sum<size_t>>(span<size_t>(container), span<size_t>(container2), true, sum<size_t>(), 0);
-
-  auto endt = std::chrono::steady_clock::now();
-
-  std::cout << "seq: " << double(std::chrono::duration_cast<std::chrono::nanoseconds>(midt - startt).count()) << std::endl;
-  std::cout << "par: " << double(std::chrono::duration_cast<std::chrono::nanoseconds>(endt - midt).count()) << std::endl;
-
-  //print_arr(ptr2, test_data_len);
-  //print_arr(ptr3, test_data_len);
-
-  for(size_t i = 0; i < test_data_len; ++i) {
-    if(ptr2[i] != ptr3[i]) {
-      std::cout << "fehler: " << ptr2[i] << "!=" << ptr3[i] << std::endl;
-      exit(-1);
-    }
-  }
-
-  // Done
-  return 0;
 }
 // End Namespace
 }
--- test/prefix_sum_tests.cpp
+++ test/prefix_sum_tests.cpp
@@ -28,20 +28,15 @@ TEST(PrefixSum, correct_sum) {
     // Alloc memory.
     //auto container = make_container<size_t>(test_data_len);
     std::cout << "Intitializing container." << std::endl;
-    //container<size_t> container {1,2,3,4,5,6,7,8,9};
+    // container<size_t> container = {1,2,3,4,5,6,7,8,9};
     auto container = make_container<size_t>(test_data_len);
     auto container2 = make_container<size_t>(container.size());
     auto container3 = make_container<size_t>(container.size());
-    /*auto ptr = container.begin();
-    auto ptr2 = container2.begin();
-    auto ptr3 = container3.begin();
-    */
 
     std::cout << "Generating random numbers." << std::endl;
     for(size_t i = 0; i < test_data_len; ++i) {
     	container[i] = rand();
     }
-
 
     std::cout << "random generation done" << std::endl;
 
@@ -49,15 +44,77 @@ TEST(PrefixSum, correct_sum) {
 
     auto startt = std::chrono::steady_clock::now();
 
+    auto oper = sum<size_t>();
+
     std::cout << "Computing sequentially." << std::endl;
     seq_prefix_sum<size_t, sum<size_t>>(span<size_t>(container),
-            span<size_t>(container3), true, sum<size_t>(), 0);;
+            span<size_t>(container3), true, oper, 0);;
 
     auto midt = std::chrono::steady_clock::now();
 
     std::cout << "Computing parallelly." << std::endl;
     par_prefix_sum<size_t, sum<size_t>>(span<size_t>(container),
-            span<size_t>(container2), true, sum<size_t>(), 0);
+            span<size_t>(container2), true, oper, 0);
+
+    auto endt = std::chrono::steady_clock::now();
+
+    std::cout << "seq: " << double(std::chrono::duration_cast<std::chrono::nanoseconds>(midt - startt).count()) << std::endl;
+    std::cout << "par: " << double(std::chrono::duration_cast<std::chrono::nanoseconds>(endt - midt).count()) << std::endl;
+
+    //print_arr(ptr2, test_data_len);
+    //print_arr(ptr3, test_data_len);
+    /*std::cout << container2 << std::endl;
+    std::cout << container3 << std::endl;*/
+    for(size_t i=0; i < container.size(); ++i) {
+        // std::cout << "Index: " << i << ", value: " << container2[i] << std::endl;
+        ASSERT_EQ(container2[i], container3[i]);
+    }
+    std::cout << std::endl;
+}
+
+TEST(PrefixSum, correct_max) {
+
+    srand(time(NULL));
+
+    // Number of CPUs.
+    //const size_t n_cpus = std::thread::hardware_concurrency();
+    //omp_set_num_threads(n_cpus);
+
+    // Calc prefixsum with threads.
+    const size_t len = 36;
+    const size_t test_data_len = len*1024*1024;
+
+    std::cout << "memory: " << (test_data_len * sizeof(size_t) / 1024.0 / 1024.0) << "MiB" << std::endl;
+
+    // Alloc memory.
+    //auto container = make_container<size_t>(test_data_len);
+    std::cout << "Intitializing container." << std::endl;
+    //container<size_t> container = {1,2,3,4,5,6,7,8,9};
+    auto container = make_container<size_t>(test_data_len);
+    auto container2 = make_container<size_t>(container.size());
+    auto container3 = make_container<size_t>(container.size());
+/*
+    std::cout << "Generating random numbers." << std::endl;
+    for(size_t i = 0; i < test_data_len; ++i) {
+    	container[i] = rand();
+    }
+
+    std::cout << "random generation done" << std::endl;
+*/
+
+    auto startt = std::chrono::steady_clock::now();
+
+    auto oper = max<size_t>();
+
+    std::cout << "Computing sequentially." << std::endl;
+    seq_prefix_sum<size_t, max<size_t>>(span<size_t>(container),
+            span<size_t>(container3), true, oper, 0);;
+
+    auto midt = std::chrono::steady_clock::now();
+
+    std::cout << "Computing parallelly." << std::endl;
+    par_prefix_sum<size_t, max<size_t>>(span<size_t>(container),
+            span<size_t>(container2), true, oper, 0);
 
     auto endt = std::chrono::steady_clock::now();
 
