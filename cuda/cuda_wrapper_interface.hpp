8c24ed8734f65db79ca6c4195aef8eed04d79cc8
Marvin Löbel <loebel.marvin@gmail.com>
2018-12-17 20:06:10 +0100

    register submodule correctly

--- cuda/cuda_wrapper_interface.hpp
+++ cuda/cuda_wrapper_interface.hpp
@@ -3,7 +3,7 @@
 #include <cstddef>
 #include <cstdint>
 
-void* allocate_cuda_buffer(size_t size);
+// void* allocate_cuda_buffer(size_t size);
 void* allocate_managed_cuda_buffer(size_t size);
 void free_cuda_buffer(void* ptr);
 
