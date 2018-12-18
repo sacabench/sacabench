6b531f1fed8f54f9476bd31bbb885b96dd76dd84
@foot <hermann.foot@tu-dortmund.de>
2018-12-18 11:53:27 +0100

    Added ISA init

--- cuda/prefix_doubler.cu
+++ cuda/prefix_doubler.cu
@@ -3,6 +3,30 @@
 #include "cub-1.8.0/cub/cub.cuh"
 
 
+#define NUM_BLOCKS 2
+#define NUM_THREADS_PER_BLOCK 4
+
+
+struct Max_without_branching
+{
+    template <typename T>
+    CUB_RUNTIME_FUNCTION __forceinline__ __device__
+    T operator()(const T &x, const T &y) const {
+        return (x ^ ((x ^ y) & -(x < y)));
+    }
+};
+
+struct Compare_first_char
+{
+public:  
+    Compare_first_char(const char* _text) : text(_text) {};
+    const char* text;
+    template <typename index>
+    CUB_RUNTIME_FUNCTION __forceinline__ __device__
+    bool operator()(const index &x, const index &y) const {
+        return text[x]<text[y];
+    }
+};
 
     __global__
     static void initialize_sa_gpu(int n, int*  sa) {
@@ -15,79 +39,171 @@
 
     }
 
+    __global__
+    static void copy_to_array(int* in, int* out, int n) {
 
-    static void inital_sorting(const char* text, int* sa, int n) {
+        int index = blockIdx.x * blockDim.x + threadIdx.x;
+        int stride = blockDim.x * gridDim.x;
+        for (int i = index; i < n; i+=stride) {
+            in[i] = out[i];
+        }
+
+    }
 
 
-    char *testText;
-     //Tatsächliche Werte
+    static void inital_sorting(char* text, int* sa, int* aux, int n) {
+
+
+     //Actual values
     char  *keys_out;     // e.g., [        ...        ]
-    //Indizes
-    int  *values_out;   // e.g., [        ...        ]
-
 
 
     // Allocate Unified Memory – accessible from CPU or GPU
-    cudaMallocManaged(&testText, n*sizeof(char));
     cudaMallocManaged(&keys_out, n*sizeof(char));
-    cudaMallocManaged(&values_out, n*sizeof(int));
 
-    //TODO: Outside!
-    memset(testText, 0, n*sizeof(char));
-    cudaMemcpy(testText, text, n*sizeof(char), cudaMemcpyHostToDevice);
 
     // Determine temporary device storage requirements
     void     *d_temp_storage = NULL;
     size_t   temp_storage_bytes = 0;
 
     cub::DeviceRadixSort::SortPairs(d_temp_storage, temp_storage_bytes,
-        testText, keys_out, sa, values_out, n);
+        text, keys_out, sa, aux, n);
     // Allocate temporary storage
     cudaMalloc(&d_temp_storage, temp_storage_bytes);
 
 
     // Run sorting operation
     cub::DeviceRadixSort::SortPairs(d_temp_storage, temp_storage_bytes,
-        testText, keys_out, sa, values_out, n);
+        text, keys_out, sa, aux, n);
 
 
     cudaDeviceSynchronize();
 
     
-    for(int i = 0 ; i< n ; ++i) {
-        sa[i]=values_out[i];
+
+
     }
+
+    template <typename OP>
+void prefix_sum_cub_inclusive(int* array, OP op, int n)
+{
+        //Indices
+        int  *values_out;   // e.g., [        ...        ]
+
+    
+    
+        // Allocate Unified Memory – accessible from CPU or GPU
+        cudaMallocManaged(&values_out, n*sizeof(int));
+    
+        // Determine temporary device storage requirements
+        void     *d_temp_storage = NULL;
+        size_t   temp_storage_bytes = 0;
+        
+        cub::DeviceScan::InclusiveScan(d_temp_storage, temp_storage_bytes, array, values_out,op, n);
+        // Allocate temporary storage
+        cudaMalloc(&d_temp_storage, temp_storage_bytes);
+        // Run exclusive prefix sum
+        cub::DeviceScan::InclusiveScan(d_temp_storage, temp_storage_bytes, array, values_out,op, n);
+
+        cudaDeviceSynchronize();
+
+        copy_to_array<<<NUM_BLOCKS,NUM_THREADS_PER_BLOCK>>>(array,values_out,n);
+
+
+}
+    template <typename Comp>
+    __global__
+    void fill_aux_for_isa(int* sa, int* aux, int n, Comp comp) {
+
+        int index = blockIdx.x * blockDim.x + threadIdx.x;
+        int stride = blockDim.x * gridDim.x;
+
+        if(index == 0) {
+            aux[0]=0;
+        }
+
+        for (int i = index+1; i < n; i+=stride) {
+            aux[i] = i * (comp(sa[i - 1], sa[i]) != 0);
+        }
+    }
+    __global__
+    void scatter_to_isa(int* isa, int* aux,int* sa, int n) {
+
+        int index = blockIdx.x * blockDim.x + threadIdx.x;
+        int stride = blockDim.x * gridDim.x;
+
+        for (int i = index; i < n; i+=stride) {
+            isa[sa[i]]=aux[i];
+        }
+    }
+
+    template <typename Comp>
+    void initialize_isa(int* isa, int* sa, int* aux, int n, Comp comp) {
+
+        fill_aux_for_isa<<<NUM_BLOCKS,NUM_THREADS_PER_BLOCK>>>(sa,aux,n, comp);
+
+        cudaDeviceSynchronize();
+
+        Max_without_branching max;
+
+        prefix_sum_cub_inclusive(aux,max, n);
+
+        cudaDeviceSynchronize();
+
+        scatter_to_isa<<<NUM_BLOCKS,NUM_THREADS_PER_BLOCK>>>(isa,aux,sa,n);
+
+        cudaDeviceSynchronize();
 
     }
 
 
     static void prefix_doubling_gpu(const char* text, int* out_sa, int n) {
         
+        char* gpu_text;
         int* sa;
         //Wofür??
         int* isa_container;
         int* aux_container;
+
+
+        cudaMallocManaged(&gpu_text, n*sizeof(char));
+
         cudaMallocManaged(&sa, n*sizeof(int));
         cudaMallocManaged(&isa_container, n*sizeof(int));
         cudaMallocManaged(&aux_container, n*sizeof(int));
 
-        cudaDeviceSynchronize();
-
-        initialize_sa_gpu<<<2,4>>>(n, sa);
-
-
-        cudaDeviceSynchronize();
-
-
-        inital_sorting(text, sa, n);
+        //Copy text to GPU
+        memset(gpu_text, 0, n*sizeof(char));
+        cudaMemcpy(gpu_text, text, n*sizeof(char), cudaMemcpyHostToDevice);
 
         cudaDeviceSynchronize();
+
+        initialize_sa_gpu<<<NUM_BLOCKS,NUM_THREADS_PER_BLOCK>>>(n, sa);
+
+
+        cudaDeviceSynchronize();
+
+
+        inital_sorting(gpu_text, sa, aux_container, n);
+
+        cudaDeviceSynchronize();
+
+        copy_to_array<<<NUM_BLOCKS,NUM_THREADS_PER_BLOCK>>>(sa,aux_container,n);
+        cudaDeviceSynchronize();
+
+
+        Compare_first_char comp(gpu_text);
+
+        initialize_isa(out_sa,sa,aux_container,n, comp);
+
+        cudaDeviceSynchronize();
+
         std::cout<<std::endl;
+        std::cout<<"ISA: ";
         for(int i = 0 ; i< n ; ++i) {
-            std::cout<<sa[i]<<", "<<std::endl;
+            std::cout<<out_sa[sa[i]]<<", ";
         }
-
-
+        std::cout<<std::endl;
 
         /*
         std::cout<<"Hallo"<<std::endl;
