2c2de4c727f524011fd27e21a9b8a3d245478bae
David Piper <david.piper@udo.edu>
2019-01-20 15:49:19 +0100

    Fixed a bug where the path of the shell script was wrong.

--- zbmessung/automation.sh
+++ zbmessung/automation.sh
@@ -9,12 +9,8 @@ currentdir=$(pwd)
 # Create a temporary directory and save the filename.
 tempdir=$(dirname $(mktemp -d))
 
-# Get path to this script. 
-# see: https://stackoverflow.com/a/1638397
-# Absolute path to this script, e.g. /home/user/bin/foo.sh
-script=$(readlink "$0")
-# Absolute path this script is in, thus /home/user/bin
-scriptpath=$(dirname "$script")
+# Get path to this script. This script is called from build directory.
+scriptpath="../zbmessung"
 
 # Copy sqlplot directory to previously generated temporary directory.
 cp -R $scriptpath/sqlplot $tempdir
@@ -32,4 +28,5 @@ mv *.pdf $pdf_dest_path
 cd $currentdir
 
 # Delete sqlplot directory in temporary directory.
-rm -rf $tempdir/sqlplot
+#rm -rf $tempdir/sqlplot
+open $tempdir/sqlplot
