4f352b49521177d0d64913fb2e5dc0cac56ed68f
David Piper <david.piper@udo.edu>
2019-01-29 12:10:13 +0100

    Improved python script to create result files. It now uses predefined keys instead of strings to save values from json. Also the creating of the final string is better, the values ar no longer set by hand but the keys in the previously created dictionary serve as the identifier for the strings.

--- zbmessung/sqlplot/json_to_result_converter.py
+++ zbmessung/sqlplot/json_to_result_converter.py
@@ -61,11 +61,52 @@ def get_processor_info():
         return name
     return ""
 
+class ResultKeys:
+    algo = "algo"
+    algorithm_name = "algorithm_name"
+    id = "id"
+    rep = "rep"
+    rep_id = "rep_id"
+    phases = "phases"
+    phase_id = "phase_id"
+    phase_name = "phase_name"
+    extra_sentinels = "extra_sentinels"
+    input = "input"
+    prefix = "prefix"
+    thread_count = "thread_count"
+    sa_index_bit_size = "sa_index_bit_size"
+    text_size = "text_size"
+    memOff = "memOff"
+    memFinal = "memFinal"
+    memPeak = "memPeak"
+    threads = "threads"
+    time = "time"	
+
+def buildResultString(data):
+    """
+    Creates a line for the algorithm result text file.
+
+    Parameters
+    ----------
+    dict : dictionary
+        The dictionary which contains the data for a repititon of an algorithm.
+
+    Returns
+    ----------
+        A string which represents one line of the algorithm result text file.
+    """
+
+    content = "RESULT\t"
+    for key, value in data.items():
+        content += "{}={}\t".format(key, value)
+    content += "\n"
+    return content
+
 ########################################
 # PROCESSING ALGORITHM DATA
 ########################################
 
-def extractAlgorithmDataFromDictionary(dict, algorithmID, repetitionID):    
+def extractAlgorithmDataFromDictionary(dict, algorithmID, repititionCount, repetitionID):    
     """
     Processes the given dictionary and returns a new dictionary.
     This new dicitionary contains the extracted data needed to create a line in the algorithm result file.
@@ -86,25 +127,24 @@ def extractAlgorithmDataFromDictionary(dict, algorithmID, repetitionID):
     
     data = {}
     
-    data["algorithmID"] = algorithmID
-    data["repetitionID"] = repetitionID
+    data[ResultKeys.id] = algorithmID
+    data[ResultKeys.rep] = repititionCount
+    data[ResultKeys.rep_id] = repetitionID
 
-    data["title"] = dict["title"]
-    data["memFinal"] = dict["memFinal"]
-    data["memOff"] = dict["memOff"]
-    data["memPeak"] = dict["memPeak"]
+    data[ResultKeys.algo] = dict["title"]
+    data[ResultKeys.memFinal] = dict["memFinal"]
+    data[ResultKeys.memOff] = dict["memOff"]
+    data[ResultKeys.memPeak] = dict["memPeak"]
 
     stats = dict["stats"]
     for statsDict in stats:
         if statsDict["key"] == "input_file":
             fullInputPath = statsDict["value"]
-            data["inputFile"] = os.path.basename(fullInputPath)
-        if statsDict["key"] == "repetitions":
-            data["repetitionCount"] = statsDict["value"]
+            data[ResultKeys.input] = os.path.basename(fullInputPath)
         if statsDict["key"] == "prefix":
-            data["prefix"] = statsDict["value"]
+            data[ResultKeys.prefix] = statsDict["value"]
         if statsDict["key"] == "thread_count":
-            data["thread_count"] = statsDict["value"]
+            data[ResultKeys.thread_count] = statsDict["value"]
 
     # Access first item in list of subs to get needed values. 
     # Every item of sub list contains these values.
@@ -112,18 +152,18 @@ def extractAlgorithmDataFromDictionary(dict, algorithmID, repetitionID):
     exampleStats = exampleSub["stats"]
     for statsDict in exampleStats:
         if statsDict["key"] == "extra_sentinels":
-            data["extraSentinels"] = statsDict["value"]
+            data[ResultKeys.extra_sentinels] = statsDict["value"]
         if statsDict["key"] == "sa_index_bit_size":
-            data["saIndexBitSize"] = statsDict["value"]
+            data[ResultKeys.sa_index_bit_size] = statsDict["value"]
         if statsDict["key"] == "text_size":
-            data["textSize"] = statsDict["value"]
+            data[ResultKeys.text_size] = statsDict["value"]
     
     timeDict = dict["sub"][0]["sub"]
     for timeEntry in timeDict:
         if timeEntry["title"] == "Algorithm":
             timeEnd = timeEntry["timeEnd"]
             timeStart = timeEntry["timeStart"]
-            data["time"] = timeEnd - timeStart
+            data[ResultKeys.time] = timeEnd - timeStart
 
     numberOfPhases = 0
     allPhases = dict["sub"][0]["sub"]
@@ -132,45 +172,9 @@ def extractAlgorithmDataFromDictionary(dict, algorithmID, repetitionID):
         if len(phases["sub"]) > 0:
             algorithmPhases = phases["sub"]
             numberOfPhases = len(algorithmPhases)
-    data["numberOfPhases"] = numberOfPhases
+    data[ResultKeys.phases] = numberOfPhases
 
     return data
-            
-def buildAlgorithmResultString(data):
-    """
-    Creates a line for the algorithm result text file.
-
-    Parameters
-    ----------
-    dict : dictionary
-        The dictionary which contains the data for a repititon of an algorithm.
-
-    Returns
-    ----------
-        A string which represents one line of the algorithm result text file.
-    """
-
-    content = "RESULT\t"
-    content += "algo={}\t".format(data["title"])
-    content += "algorithm_name={}\t".format(data["title"])
-    content += "id={}\t".format(data["algorithmID"])
-    content += "rep={}\t".format(data["repetitionCount"])
-    content += "rep_id={}\t".format(data["repetitionID"])
-    content += "phases={}\t".format(data["numberOfPhases"])
-    content += "extra_sentinels={}\t".format(data["extraSentinels"])
-    content += "input={}\t".format(data["inputFile"])
-    content += "prefix={}\t".format(data["prefix"])
-    content += "thread_count={}\t".format(data["thread_count"])
-    content += "sa_index_bit_size={}\t".format(data["saIndexBitSize"])
-    content += "text_size={}\t".format(data["textSize"])
-    content += "memOff={}\t".format(data["memOff"])
-    content += "memFinal={}\t".format(data["memFinal"])
-    content += "memPeak={}\t".format(data["memPeak"])
-    # TODO: Number of threads is currently not included in input JSON. 
-    content += "threads=1\t" 
-    content += "time={}\n".format(data["time"])
-
-    return content	
 
 ########################################
 # PROCESSING PHASES DATA
@@ -198,46 +202,20 @@ def extractPhaseDataFromDictionary(dict, algorithmID, repetitionID, phaseID):
     """
     
     data = {}
-    data["algorithmID"] = algorithmID
-    data["repetitionID"] = repetitionID
-    data["phaseID"] = phaseID
+    data[ResultKeys.id] = algorithmID
+    data[ResultKeys.rep_id] = repetitionID
+    data[ResultKeys.phase_id] = phaseID
 
-    data["title"] = dict["title"]
-    data["memFinal"] = dict["memFinal"]
-    data["memOff"] = dict["memOff"]
-    data["memPeak"] = dict["memPeak"]
+    data[ResultKeys.phase_name] = dict["title"]
+    data[ResultKeys.memFinal] = dict["memFinal"]
+    data[ResultKeys.memOff] = dict["memOff"]
+    data[ResultKeys.memPeak] = dict["memPeak"]
 
     timeEnd = dict["timeEnd"]
     timeStart = dict["timeStart"]
-    data["time"] = timeEnd - timeStart
+    data[ResultKeys.time] = timeEnd - timeStart
 
     return data	
-
-def buildPhasesResultString(data):
-    """
-    Creates a line for the phases result text file.
-
-    Parameters
-    ----------
-    dict : dictionary
-        The dictionary which contains the data for a phase.
-
-    Returns
-    ----------
-        A string which represents one line of the phases result text file.
-    """
-
-    content = "RESULT\t"
-    content += "id={}\t".format(data["algorithmID"])
-    content += "rep_id={}\t".format(data["repetitionID"])
-    content += "phase_id={}\t".format(data["phaseID"])
-    content += "phase_name={}\t".format(data["title"])
-    content += "memOff={}\t".format(data["memOff"])
-    content += "memFinal={}\t".format(data["memFinal"])
-    content += "memPeak={}\t".format(data["memPeak"])
-    content += "time={}\n".format(data["time"])
-
-    return content
 
 ########################################
 # PROCESS FILE
@@ -259,6 +237,7 @@ def convertAndSaveData(dict, path):
     phasesFileContent = ""
 
     currentrepetitionNumber = 0
+    repetitionCount = 0
     currentAlgorithmNumber = 0
     currentPhaseNumber = 0
 
@@ -269,6 +248,8 @@ def convertAndSaveData(dict, path):
         # Increase id of each algorithm.
         currentAlgorithmNumber += 1
 
+        repetitionCount = len(algorithm)
+
         for repetition in algorithm:
             # Increase id of current repetition when processing a new repetition of the current algorith.
             currentrepetitionNumber += 1
@@ -276,8 +257,8 @@ def convertAndSaveData(dict, path):
             # Set id of current phase to 0 when processing an new repititon.
             currentPhaseNumber = 0
 
-            algorithmDataDict = extractAlgorithmDataFromDictionary(repetition, currentAlgorithmNumber, currentrepetitionNumber)
-            algorithmFileContent += buildAlgorithmResultString(algorithmDataDict)
+            algorithmDataDict = extractAlgorithmDataFromDictionary(repetition, currentAlgorithmNumber, repetitionCount, currentrepetitionNumber)
+            algorithmFileContent += buildResultString(algorithmDataDict)
 
             # List of sub contains only one element.
             allPhases = repetition["sub"][0]["sub"]
@@ -291,10 +272,9 @@ def convertAndSaveData(dict, path):
                         currentPhaseNumber += 1
 
                         phaseDataDict = extractPhaseDataFromDictionary(phase, currentAlgorithmNumber, currentrepetitionNumber, currentPhaseNumber)
-                        phasesFileContent += buildPhasesResultString(phaseDataDict)
+                        phasesFileContent += buildResultString(phaseDataDict)
 
     algorithmFilePath = "{}/result_algorithm.txt".format(path)
-    print("Saving {} to {}".format(algorithmFileContent, algorithmFilePath))
     writeFile(algorithmFilePath, algorithmFileContent)
 
     phasesFilePath = "{}/result_phases.txt".format(path)
