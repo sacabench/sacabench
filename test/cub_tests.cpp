5197d9bfb89348a26ee1925feb5f6428048c1c04
@foot <hermann.foot@tu-dortmund.de>
2019-03-06 16:45:49 +0100

    fixed broken cub test

--- test/cub_tests.cpp
+++ test/cub_tests.cpp
@@ -12,27 +12,15 @@ TEST(Cub, prefix_sum) {
     // Declare, allocate, and initialize device-accessible pointers for input and output
     size_t   num_items = 7;
     uint64_t* d_in = allocate_managed_cuda_buffer_of<uint64_t>(num_items);
-    uint64_t* d_out = allocate_managed_cuda_buffer_of<uint64_t>(num_items);
     d_in[0] = 8;
     d_in[1] = 6;
-    for(size_t i=0; i < num_items; ++i) {
-        std::cout << d_in[i] << ", ";
-    }
-    std::cout << std::endl;
     /*
     uint64_t d_in[] = {8, 6, 7, 5, 3, 0, 9};
-    uint64_t d_out[] = {0, 0, 0, 0, 0, 0, 0};
-
+    uint64_t d_out[] = {0, 0, 0, 0, 0, 0, 0}
 
     */
     // Run exclusive prefix sum
-    exclusive_sum(d_in, d_out, num_items);
-
-    for(size_t i=0; i < num_items; ++i) {
-        std::cout << d_in[i] << ", ";
-    }
-    std::cout << std::endl;
+    exclusive_sum(d_in, num_items);
 
     free_cuda_buffer(d_in);
-    free_cuda_buffer(d_out);
 }
