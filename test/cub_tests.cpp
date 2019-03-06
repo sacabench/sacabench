4317e55e2fadaf6e3d4f2e17ea1c6255c97a0307
@foot <hermann.foot@tu-dortmund.de>
2019-03-06 17:05:06 +0100

    fixed broken cub test again

--- test/cub_tests.cpp
+++ test/cub_tests.cpp
@@ -7,20 +7,24 @@
 #include <gtest/gtest.h>
 #include <util/alphabet.hpp>
 #include <cuda_wrapper_interface.hpp>
+#include <check_for_gpu_interface.hpp>
 
 TEST(Cub, prefix_sum) {
-    // Declare, allocate, and initialize device-accessible pointers for input and output
-    size_t   num_items = 7;
-    uint64_t* d_in = allocate_managed_cuda_buffer_of<uint64_t>(num_items);
-    d_in[0] = 8;
-    d_in[1] = 6;
-    /*
-    uint64_t d_in[] = {8, 6, 7, 5, 3, 0, 9};
-    uint64_t d_out[] = {0, 0, 0, 0, 0, 0, 0}
 
-    */
-    // Run exclusive prefix sum
-    exclusive_sum(d_in, num_items);
+    if(cuda_GPU_available()) {
+        // Declare, allocate, and initialize device-accessible pointers for input and output
+        size_t   num_items = 7;
+        uint64_t* d_in = allocate_managed_cuda_buffer_of<uint64_t>(num_items);
+        d_in[0] = 8;
+        d_in[1] = 6;
+        /*
+        uint64_t d_in[] = {8, 6, 7, 5, 3, 0, 9};
+        uint64_t d_out[] = {0, 0, 0, 0, 0, 0, 0}
 
-    free_cuda_buffer(d_in);
+        */
+        // Run exclusive prefix sum
+        exclusive_sum(d_in, num_items);
+
+        free_cuda_buffer(d_in);
+        }
 }
